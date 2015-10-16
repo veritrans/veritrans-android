15efad1cde4d8057d4e2bee37ec0405ed65a0a6d
chetan bhalerao <chetan@leftshift.io>
2015-10-16 15:48:34 +0530

    created user detail fragment

--- src/main/java/id/co/veritrans/sdk/activities/UserDetailsActivity.java
+++ src/main/java/id/co/veritrans/sdk/activities/UserDetailsActivity.java
@@ -1,8 +1,10 @@
-package id.co.veritrans.sdk;
+package id.co.veritrans.sdk.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import id.co.veritrans.sdk.R;
 
 public class UserDetailsActivity extends AppCompatActivity {
 
---
+++ src/main/java/id/co/veritrans/sdk/core/VeritransRestAdapter.java
@@ -0,0 +1,7 @@
+package id.co.veritrans.sdk.core;
+
+/**
+ * Created by chetan on 16/10/15.
+ */
+public class VeritransRestAdapter {
+}
