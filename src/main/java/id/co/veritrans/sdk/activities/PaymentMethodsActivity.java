a5427f84fdcb097be7b46539c9ce6c10ea293f2f
shivam340 <shivam@leftshift.io>
2015-10-16 19:15:34 +0530

    added string for payment methods

--- src/main/java/id/co/veritrans/sdk/activities/PaymentMethodsActivity.java
+++ src/main/java/id/co/veritrans/sdk/activities/PaymentMethodsActivity.java
@@ -1,7 +1,9 @@
-package id.co.veritrans.sdk;
+package id.co.veritrans.sdk.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import id.co.veritrans.sdk.R;
 
 /**
  * Created by shivam on 10/16/15.
