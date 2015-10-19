fe09da9983c1e5bac1579b0cca9c5c7fd13df790
shivam340 <shivam@leftshift.io>
2015-10-19 16:01:02 +0530

    created adapter for payment methods.

---
+++ src/main/java/id/co/veritrans/sdk/adapters/PaymentMethodsAdapter.java
@@ -0,0 +1,87 @@
+package id.co.veritrans.sdk.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import id.co.veritrans.sdk.R;
+import id.co.veritrans.sdk.models.PaymentMethodsModel;
+import id.co.veritrans.sdk.widgets.TextViewFont;
+
+/**
+ * Created by shivam on 10/19/15.
+ */
+public class PaymentMethodsAdapter extends RecyclerView.Adapter<PaymentMethodsAdapter
+        .PaymentViewHolder> {
+
+
+    private static final String TAG = PaymentMethodsAdapter.class.getSimpleName();
+
+    private static Context mContext;
+    private ArrayList<PaymentMethodsModel> data = null;
+
+
+    public PaymentMethodsAdapter(Context mContext, ArrayList<PaymentMethodsModel> data) {
+        this.mContext = mContext;
+        this.data = data;
+        Log.d(TAG , "setting adapter");
+
+    }
+
+
+    @Override
+    public PaymentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_row_payment_methods, parent, false);
+
+        PaymentViewHolder paymentViewHolder = new PaymentViewHolder(view);
+        return paymentViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(PaymentViewHolder holder, int position) {
+        holder.mImageView.setImageResource(data.get(position).getImageId());
+        holder.name.setText(data.get(position).getName());
+
+        Log.d(TAG , "name is "+data.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+
+    /**
+     * public static view holder class.
+     */
+    public static class PaymentViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+
+        TextViewFont name;
+        ImageView mImageView;
+
+        public PaymentViewHolder(View itemView) {
+            super(itemView);
+            name = (TextViewFont) itemView.findViewById(R.id.text_payment_method_name);
+            mImageView = (ImageView) itemView.findViewById(R.id.img_payment_method_icon);
+            itemView.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(mContext, "clicked on "+getAdapterPosition(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
