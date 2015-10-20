d40751d7bd358219c814462e86863c55867b2933
shivam340 <shivam@leftshift.io>
2015-10-20 15:33:59 +0530

    added collapsing effect for payment amount

--- src/main/java/id/co/veritrans/sdk/activities/PaymentMethodsActivity.java
+++ src/main/java/id/co/veritrans/sdk/activities/PaymentMethodsActivity.java
@@ -1,10 +1,13 @@
 package id.co.veritrans.sdk.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -21,7 +24,10 @@ public class PaymentMethodsActivity extends AppCompatActivity {
     private ArrayList<PaymentMethodsModel> data = new ArrayList<>();
     private static final String TAG = PaymentMethodsActivity.class.getSimpleName();
 
+    private CollapsingToolbarLayout mCollapsingToolbarLayout = null;
+    private Toolbar mToolbar = null;
 
+    private TextView mTextViewPayableAmount = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,16 @@ public class PaymentMethodsActivity extends AppCompatActivity {
     private void setUpPaymentMethods() {
         initialiseAdapterData();
 
+
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout)
+                findViewById(R.id.collapsing_toolbar_layout);
+        mCollapsingToolbarLayout.setTitle("DemoTitle");
+       
+
+        /*mTextViewPayableAmount = (TextView) findViewById(R.id.text_payable_amount);
+        mTextViewPayableAmount.setText(getResources().getString(R.string.payable_amount));
+        */
+
         mRecyclerView = (RecyclerView) findViewById(R.id.rv_payment_methods);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(
@@ -43,6 +59,12 @@ public class PaymentMethodsActivity extends AppCompatActivity {
         PaymentMethodsAdapter paymentMethodsAdapter = new
                 PaymentMethodsAdapter(getApplicationContext(), data);
         mRecyclerView.setAdapter(paymentMethodsAdapter);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("Tool Bar");
+        mToolbar.setSubtitle("Subtitle");
+        setSupportActionBar(mToolbar);
+
     }
 
     private void initialiseAdapterData() {
@@ -50,7 +72,7 @@ public class PaymentMethodsActivity extends AppCompatActivity {
         String[] names = getResources().getStringArray(R.array.payment_methods);
         int[] icons = new int[names.length];
 
-        Log.d(TAG, "there are total "+names.length+" payment methods available.");
+        Log.d(TAG, "there are total " + names.length + " payment methods available.");
 
         for (int i = 0; i < names.length; i++) {
             icons[i] = R.drawable.ic_launcher;
