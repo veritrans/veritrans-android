623e5925392576b2f5c9758ce5af857fc9ad4585
shivam340 <shivam@leftshift.io>
2015-10-21 15:21:14 +0530

    #VER-27 #time 5h 20m #close fixed collapsing title issue

--- src/main/java/id/co/veritrans/sdk/activities/PaymentMethodsActivity.java
+++ src/main/java/id/co/veritrans/sdk/activities/PaymentMethodsActivity.java
@@ -1,11 +1,16 @@
 package id.co.veritrans.sdk.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,61 +23,147 @@ import id.co.veritrans.sdk.models.PaymentMethodsModel;
 /**
  * Created by shivam on 10/16/15.
  */
-public class PaymentMethodsActivity extends AppCompatActivity {
+public class PaymentMethodsActivity extends AppCompatActivity implements AppBarLayout
+        .OnOffsetChangedListener {
 
-    private RecyclerView mRecyclerView = null;
+
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f;
+    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.3f;
+    private static final int ALPHA_ANIMATIONS_DURATION = 200;
+
+    private boolean mIsTheTitleVisible = false;
+    private boolean mIsTheTitleContainerVisible = true;
     private ArrayList<PaymentMethodsModel> data = new ArrayList<>();
     private static final String TAG = PaymentMethodsActivity.class.getSimpleName();
 
-    private CollapsingToolbarLayout mCollapsingToolbarLayout = null;
-    private Toolbar mToolbar = null;
 
-    private TextView mTextViewPayableAmount = null;
+    //Views
+    private Toolbar mToolbar = null;
+    private TextView mSubTitle;
+    private LinearLayout mTitleContainer;
+    private AppBarLayout mAppBarLayout;
+    private FrameLayout mFrameParallax;
+    private RecyclerView mRecyclerView = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_payments_method);
         setUpPaymentMethods();
     }
 
     private void setUpPaymentMethods() {
-        initialiseAdapterData();
 
-        mCollapsingToolbarLayout = (CollapsingToolbarLayout)
-                findViewById(R.id.collapsing_toolbar_layout);
-        mCollapsingToolbarLayout.setTitle(getResources().getString(R.string.select_payment_method));
+        //initialize views
+        bindActivity();
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setup tool bar
+        mToolbar.setTitle("");
+        mAppBarLayout.addOnOffsetChangedListener(this);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.rv_payment_methods);
+
+        //hide subtitle
+        startAlphaAnimation(mSubTitle, 0, View.INVISIBLE);
+
+
+        initParallaxValues();
+
+
+        // setUp recyclerView
+        initialiseAdapterData();
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-
         PaymentMethodsAdapter paymentMethodsAdapter = new
                 PaymentMethodsAdapter(getApplicationContext(), data);
         mRecyclerView.setAdapter(paymentMethodsAdapter);
-
-
     }
 
+
+    private void bindActivity() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_payment_methods);
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        mSubTitle = (TextView) findViewById(R.id.main_textview_subtitle);
+        mTitleContainer = (LinearLayout) findViewById(R.id.main_linearlayout_title);
+        mAppBarLayout = (AppBarLayout) findViewById(R.id.main_appbar);
+        mFrameParallax = (FrameLayout) findViewById(R.id.main_framelayout_title);
+    }
+
+
+    private void initParallaxValues() {
+        CollapsingToolbarLayout.LayoutParams petBackgroundLp =
+                (CollapsingToolbarLayout.LayoutParams) mFrameParallax.getLayoutParams();
+        petBackgroundLp.setParallaxMultiplier(0.7f);
+        mFrameParallax.setLayoutParams(petBackgroundLp);
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(offset) / (float) maxScroll;
+
+        handleAlphaOnTitle(percentage);
+        handleToolbarTitleVisibility(percentage);
+    }
+
+
+    private void handleToolbarTitleVisibility(float percentage) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+
+            if (!mIsTheTitleVisible) {
+                startAlphaAnimation(mSubTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleVisible = true;
+            }
+
+        } else {
+
+            if (mIsTheTitleVisible) {
+                startAlphaAnimation(mSubTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleVisible = false;
+            }
+        }
+    }
+
+    private void handleAlphaOnTitle(float percentage) {
+        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
+            if (mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleContainerVisible = false;
+            }
+
+        } else {
+
+            if (!mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleContainerVisible = true;
+            }
+        }
+    }
+
+    public static void startAlphaAnimation(View v, long duration, int visibility) {
+        AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
+                ? new AlphaAnimation(0f, 1f)
+                : new AlphaAnimation(1f, 0f);
+
+        alphaAnimation.setDuration(duration);
+        alphaAnimation.setFillAfter(true);
+        v.startAnimation(alphaAnimation);
+    }
+
+
+    /**
+     * initialize adapter data model by dummy values.
+     */
     private void initialiseAdapterData() {
 
         String[] names = getResources().getStringArray(R.array.payment_methods);
-        int[] icons = new int[names.length];
-
         Logger.d(TAG, "there are total " + names.length + " payment methods available.");
 
         for (int i = 0; i < names.length; i++) {
-            icons[i] = R.drawable.ic_launcher;
-        }
-
-        for (int i = 0; i < names.length; i++) {
-            PaymentMethodsModel model = new PaymentMethodsModel(names[i], icons[i]);
+            PaymentMethodsModel model = new PaymentMethodsModel(names[i], R.drawable.ic_launcher);
             data.add(model);
         }
 
@@ -82,4 +173,5 @@ public class PaymentMethodsActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
 }
