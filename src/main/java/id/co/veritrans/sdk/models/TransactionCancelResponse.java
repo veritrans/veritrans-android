d4b709a11c2f4661aaa714aaac57a0fb726f7866
shivam340 <shivam@leftshift.io>
2015-10-26 15:59:58 +0530

    created models for transaction details

--- src/main/java/id/co/veritrans/sdk/models/TransactionCancelResponse.java
+++ src/main/java/id/co/veritrans/sdk/models/TransactionCancelResponse.java
@@ -3,17 +3,15 @@ package id.co.veritrans.sdk.models;
 /**
  * Created by shivam on 10/26/15.
  */
-public class TokenDetails {
+public class TransactionCancelResponse {
 
 
     /**
-     * status_message : OK, success request new token
-     * status_code : 200
-     * token_id : 481111-1114-ec66e79a-b104-4ccb-b916-bb71247ce3b1
+     * status_message : Merchant cannot modify the status of the transaction
+     * status_code : 412
      */
     private String status_message;
     private String status_code;
-    private String token_id;
 
     public void setStatus_message(String status_message) {
         this.status_message = status_message;
@@ -23,19 +21,11 @@ public class TokenDetails {
         this.status_code = status_code;
     }
 
-    public void setToken_id(String token_id) {
-        this.token_id = token_id;
-    }
-
     public String getStatus_message() {
         return status_message;
     }
 
     public String getStatus_code() {
         return status_code;
-    }
-
-    public String getToken_id() {
-        return token_id;
     }
 }
