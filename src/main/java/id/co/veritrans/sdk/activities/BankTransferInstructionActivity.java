b03f80327cb12a48f6623728d76bb489a35a5189
shivam340 <shivam@leftshift.io>
2015-10-28 11:42:48 +0530

    created activity for banktranfer instruction

---
+++ src/main/java/id/co/veritrans/sdk/activities/BankTransferInstructionActivity.java
@@ -0,0 +1,20 @@
+package id.co.veritrans.sdk.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import id.co.veritrans.sdk.R;
+
+/**
+ * Created by shivam on 10/28/15.
+ */
+public class BankTransferInstructionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_bank_transfer_instruction);
+
+    }
+}
