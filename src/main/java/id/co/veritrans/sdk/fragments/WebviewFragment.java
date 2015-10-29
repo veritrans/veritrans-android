bdf3b856d0831308520ec6e7977c37975f32cb56
chetan bhalerao <chetan@leftshift.io>
2015-10-29 19:56:49 +0530

    VER-31 #time 4h 10m worked on browser page and stuff,created progress dialog

---
+++ src/main/java/id/co/veritrans/sdk/fragments/WebviewFragment.java
@@ -0,0 +1,127 @@
+package id.co.veritrans.sdk.fragments;
+
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import id.co.veritrans.sdk.R;
+import id.co.veritrans.sdk.activities.CreditDebitCardFlowActivity;
+import id.co.veritrans.sdk.core.Logger;
+import id.co.veritrans.sdk.core.SdkUtil;
+
+public class WebviewFragment extends Fragment {
+
+    private static final String URL_PARAM = "url_param";
+
+
+
+    private String webUrl;
+    public WebView webView;
+    private Button sucBt;
+    private Button unsucBt;
+
+    public static WebviewFragment newInstance(String url ) {
+        WebviewFragment fragment = new WebviewFragment();
+        Bundle args = new Bundle();
+        args.putString(URL_PARAM, url);
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public WebviewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            webUrl = getArguments().getString(URL_PARAM);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_webview, container, false);
+        webView = (WebView)view.findViewById(R.id.webview);
+        sucBt = (Button)view.findViewById(R.id.btn_success);
+        unsucBt = (Button)view.findViewById(R.id.btn_unsuccess);
+        sucBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                trnsSuc(v);
+            }
+        });
+        unsucBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                trnsUnsuc(v);
+            }
+        });
+        initwebview();
+        webView.loadUrl(webUrl);
+        return view;
+    }
+
+    private void initwebview() {
+        SdkUtil.showProgressDialog(getActivity(), true);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        webView.setWebViewClient(new VeritransWebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+    }
+    private class VeritransWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            /*if (Uri.parse(url).getHost().equals("URL_NAME")) {
+                return false;
+            }
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);*/
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            SdkUtil.hideProgressDialog();
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            Logger.i("Url:"+url);
+            super.onPageStarted(view, url, favicon);
+            SdkUtil.showProgressDialog(getActivity(),false);
+        }
+    }
+    public void webviewBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        }
+    }
+
+    public void trnsSuc(View view){
+        PaymentTransactionStatusFragment paymentTransactionStatusFragment = PaymentTransactionStatusFragment.newInstance(true);
+        ((CreditDebitCardFlowActivity) getActivity()).replaceFragment(paymentTransactionStatusFragment,true,false);
+    }
+    public void trnsUnsuc(View view){
+        PaymentTransactionStatusFragment paymentTransactionStatusFragment = PaymentTransactionStatusFragment.newInstance(false);
+        ((CreditDebitCardFlowActivity) getActivity()).replaceFragment(paymentTransactionStatusFragment,true,false);
+    }
+}
---
+++ src/main/java/id/co/veritrans/sdk/widgets/VeritransLoadingDialog.java
@@ -0,0 +1,29 @@
+package id.co.veritrans.sdk.widgets;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import id.co.veritrans.sdk.R;
+
+/**
+ * Created by chetan on 29/10/15.
+ */
+public class VeritransLoadingDialog extends Dialog {
+    public VeritransLoadingDialog(Context context) {
+        super(context);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.layout_material_progress);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+    }
+
+}
