7a5a9e2d100f8dbef2663643e193f7a2571cffdd
shivam340 <shivam@leftshift.io>
2015-10-29 13:31:50 +0530

    added code invoke getToken api call

--- src/main/java/id/co/veritrans/sdk/core/VeritransSDK.java
+++ src/main/java/id/co/veritrans/sdk/core/VeritransSDK.java
@@ -1,8 +1,12 @@
 package id.co.veritrans.sdk.core;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
+
+import id.co.veritrans.sdk.callbacks.TokenCallBack;
+import id.co.veritrans.sdk.models.TokenRequestModel;
 
 /**
  * Created by shivam on 10/19/15.
@@ -13,6 +17,7 @@ public class VeritransSDK {
     private static final String FONTS_OPEN_SANS_REGULAR_TTF = "fonts/open_sans_regular.ttf";
     private static final String FONTS_OPEN_SANS_SEMI_BOLD_TTF = "fonts/open_sans_semibold.ttf";
     private static Context sContext = null;
+    private static Activity sActivity = null;
 
     private static Typeface typefaceOpenSansRegular = null;
     private static Typeface typefaceOpenSansSemiBold = null;
@@ -25,6 +30,10 @@ public class VeritransSDK {
     private static double sAmount = 0.0;
     private static int sPaymentMethod = Constants.PAYMENT_METHOD_NOT_SELECTED;
     private static boolean isRunning = false;
+    private static String sServerKey = null;
+    private static String sClientKey = null;
+
+
 
     private VeritransSDK() {
     }
@@ -33,11 +42,15 @@ public class VeritransSDK {
     protected static VeritransSDK getInstance(VeritransBuilder veritransBuilder) {
 
         if (veritransBuilder != null) {
+            sActivity = veritransBuilder.mActivity;
             sContext = veritransBuilder.context;
             sOrderId = veritransBuilder.orderId;
             sAmount = veritransBuilder.amount;
             sPaymentMethod = veritransBuilder.paymentMethod;
             sIsLogEnabled = veritransBuilder.enableLog;
+            sServerKey = veritransBuilder.serverKey;
+            sClientKey = veritransBuilder.clientKey;
+
             initializeFonts();
             return sVeritransSDK;
         } else {
@@ -106,4 +119,31 @@ public class VeritransSDK {
     public int getPaymentMethod() {
         return sPaymentMethod;
     }
+
+    public Context getContext() {
+        return sContext;
+    }
+
+    public String getServerKey() {
+        return sServerKey;
+    }
+
+    public String getClientKey() {
+        return sClientKey;
+    }
+
+
+    protected Activity getActivity(){
+        return sActivity;
+    }
+
+
+    /**
+     * @param activity
+     * @param tokenRequestModel
+     */
+    public void getToken(Activity activity, TokenRequestModel tokenRequestModel, TokenCallBack tokenCallBack){
+        TransactionHandler.getToken(activity, tokenRequestModel, tokenCallBack);
+    }
+
 }
