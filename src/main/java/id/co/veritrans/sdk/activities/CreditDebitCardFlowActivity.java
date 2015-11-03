3ed774adbc77afb31aa09150f533f7920c00d9d5
shivam340 <shivam@leftshift.io>
2015-11-03 16:35:35 +0530

    fixed typo

--- src/main/java/id/co/veritrans/sdk/activities/CreditDebitCardFlowActivity.java
+++ src/main/java/id/co/veritrans/sdk/activities/CreditDebitCardFlowActivity.java
@@ -238,11 +238,11 @@ public class CreditDebitCardFlowActivity extends AppCompatActivity implements To
                                 BillingAddress billingAddress;
                                 billingAddress = new BillingAddress();
                                 billingAddress.setCity(userAddress.getCity());
-                                billingAddress.setFirst_name(userDetail.getUserFullName());
-                                billingAddress.setLast_name("");
+                                billingAddress.setFirstName(userDetail.getUserFullName());
+                                billingAddress.setLastName("");
                                 billingAddress.setPhone(userDetail.getPhoneNumber());
-                                billingAddress.setCountry_code(userAddress.getCountry());
-                                billingAddress.setPostal_code(userAddress.getZipcode());
+                                billingAddress.setCountryCode(userAddress.getCountry());
+                                billingAddress.setPostalCode(userAddress.getZipcode());
                                 billingAddresses.add(billingAddress);
                             }
 
