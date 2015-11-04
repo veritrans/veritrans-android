c456fed781b6cc1b8bb162ea9f754217fd8003ba
shivam340 <shivam@leftshift.io>
2015-11-04 19:17:21 +0530

    #VER-34 #time 3h 20m displayed mandiri bill and bank transfer response

--- src/main/java/id/co/veritrans/sdk/fragments/BankTransactionStatusFragment.java
+++ src/main/java/id/co/veritrans/sdk/fragments/BankTransactionStatusFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import id.co.veritrans.sdk.R;
 import id.co.veritrans.sdk.activities.BankTransferActivity;
@@ -19,21 +20,27 @@ import id.co.veritrans.sdk.widgets.TextViewFont;
 public class BankTransactionStatusFragment extends Fragment {
 
 
-    public static final String VIRTUAL_ACCOUNT = "Virtual Account";
-    public static final String MANDIRI_BILL = "Mandiri Bill Payment";
+    private static final String VIRTUAL_ACCOUNT = "Virtual Account";
+    private static final String MANDIRI_BILL = "Mandiri Bill Payment";
+    private static final String DATA = "data";
 
-    private static TransactionResponse sPermataBankTransferResponse = null;
+    private TransactionResponse mPermataBankTransferResponse = null;
 
     private TextViewFont mTextViewAmount = null;
     private TextViewFont mTextViewOrderId = null;
     private TextViewFont mTextViewTransactionTime = null;
     private TextViewFont mTextViewBankName = null;
 
+    private ImageView mImageViewTransactionStatus = null;
+    private TextViewFont mTextViewFontTransactionStatus = null;
+    private TextViewFont mTextViewFontPaymentErrorMessage = null;
 
     public static BankTransactionStatusFragment newInstance(TransactionResponse
-                                                                    permataBankTransferResponse) {
-        sPermataBankTransferResponse = permataBankTransferResponse;
+                                                                    transactionResponse) {
         BankTransactionStatusFragment fragment = new BankTransactionStatusFragment();
+        Bundle data = new Bundle();
+        data.putSerializable(DATA, transactionResponse);
+        fragment.setArguments(data);
         return fragment;
     }
 
@@ -49,21 +56,32 @@ public class BankTransactionStatusFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         initializeViews(view);
+
+        Bundle data = getArguments();
+        mPermataBankTransferResponse = (TransactionResponse) data.getSerializable(DATA);
         initializeDataToView();
+
     }
 
     private void initializeViews(View view) {
+
         mTextViewAmount = (TextViewFont) view.findViewById(R.id.text_amount);
         mTextViewOrderId = (TextViewFont) view.findViewById(R.id.text_order_id);
         mTextViewBankName = (TextViewFont) view.findViewById(R.id.text_payment_type);
         mTextViewTransactionTime = (TextViewFont) view.findViewById(R.id.text_transaction_time);
+
+        mImageViewTransactionStatus = (ImageView) view.findViewById(R.id.img_transaction_status);
+        mTextViewFontTransactionStatus = (TextViewFont) view.findViewById(R.id
+                .text_transaction_status);
+        mTextViewFontPaymentErrorMessage = (TextViewFont) view.findViewById(R.id
+                .text_payment_error_message);
+
     }
 
 
     private void initializeDataToView() {
 
-
-        if (sPermataBankTransferResponse != null) {
+        if (mPermataBankTransferResponse != null) {
 
             if (getActivity() != null) {
 
@@ -76,12 +94,40 @@ public class BankTransactionStatusFragment extends Fragment {
 
             }
 
+            mTextViewTransactionTime.setText(mPermataBankTransferResponse.getTransactionTime());
+            mTextViewOrderId.setText(mPermataBankTransferResponse.getOrderId());
+            mTextViewAmount.setText(mPermataBankTransferResponse.getGrossAmount());
 
-            mTextViewTransactionTime.setText(sPermataBankTransferResponse.getTransactionTime());
-            mTextViewOrderId.setText(sPermataBankTransferResponse.getOrderId());
-            mTextViewAmount.setText(sPermataBankTransferResponse.getGrossAmount());
+            if (mPermataBankTransferResponse.getTransactionStatus().contains("Pending") ||
+                    mPermataBankTransferResponse.getTransactionStatus().contains("pending")) {
 
+            } else if (mPermataBankTransferResponse.getStatusCode().equalsIgnoreCase(Constants
+                    .SUCCESS_CODE_200) || mPermataBankTransferResponse.getStatusCode().
+                            equalsIgnoreCase(Constants.SUCCESS_CODE_201)) {
+
+                setUiForSuccess();
+            } else {
+
+                setUiForFailure();
+                if (getActivity() != null) {
+                    ((BankTransferActivity) getActivity()).activateRetry();
+                }
+            }
         }
+    }
+
+
+    private void setUiForFailure() {
+        mImageViewTransactionStatus.setImageResource(R.drawable.ic_failure);
+        mTextViewFontTransactionStatus.setText(getString(R.string.payment_unsuccessful));
+        mTextViewFontPaymentErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    private void setUiForSuccess() {
+
+        mImageViewTransactionStatus.setImageResource(R.drawable.ic_successful);
+        mTextViewFontTransactionStatus.setText(getString(R.string.payment_successful));
+        mTextViewFontPaymentErrorMessage.setVisibility(View.GONE);
     }
 
 }
