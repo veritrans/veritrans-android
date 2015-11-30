92815422f3303fd69fe31b0bcee251129f486b09
shivam340 <shivam@leftshift.io>
2015-11-30 17:02:54 +0530

    updated model

--- src/main/java/id/co/veritrans/sdk/models/IndomaretRequestModel.java
+++ src/main/java/id/co/veritrans/sdk/models/IndomaretRequestModel.java
@@ -2,6 +2,8 @@ package id.co.veritrans.sdk.models;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 /**
  * Created by shivam on 11/30/15.
  */
@@ -9,16 +11,27 @@ public class IndomaretRequestModel {
 
 
     /**
+     * email :
+     * first_name : shs
+     * last_name :
+     * phone : 8576945461
+     */
+
+    @SerializedName("customer_details")
+    private CustomerDetails customerDetails;
+    /**
+     * customer_details : {"email":"","first_name":"shs","last_name":" ","phone":"8576945461"}
+     * item_details : [{"id":"1","name":"pen","price":1000,"quantity":1}]
      * payment_type : cstore
-     * transaction_details : {"order_id":"1388","gross_amount":100000}
+     * transaction_details : {"gross_amount":"1000.0","order_id":"980c31f50f7"}
      * cstore : {"store":"indomaret","message":"mangga"}
      */
 
     @SerializedName("payment_type")
     private String paymentType;
     /**
-     * order_id : 1388
-     * gross_amount : 100000
+     * gross_amount : 1000.0
+     * order_id : 980c31f50f7
      */
 
     @SerializedName("transaction_details")
@@ -29,6 +42,18 @@ public class IndomaretRequestModel {
      */
 
     private CstoreEntity cstore;
+    /**
+     * id : 1
+     * name : pen
+     * price : 1000.0
+     * quantity : 1.0
+     */
+
+    private List<ItemDetails> item_details;
+
+    public void setCustomerDetails(CustomerDetails customerDetails) {
+        this.customerDetails = customerDetails;
+    }
 
     public void setPaymentType(String paymentType) {
         this.paymentType = paymentType;
@@ -40,6 +65,14 @@ public class IndomaretRequestModel {
 
     public void setCstore(CstoreEntity cstore) {
         this.cstore = cstore;
+    }
+
+    public void setItem_details(List<ItemDetails> item_details) {
+        this.item_details = item_details;
+    }
+
+    public CustomerDetails getCustomerDetails() {
+        return customerDetails;
     }
 
     public String getPaymentType() {
@@ -54,28 +87,11 @@ public class IndomaretRequestModel {
         return cstore;
     }
 
-    public static class TransactionDetails {
-        @SerializedName("order_id")
-        private String orderId;
-        @SerializedName("gross_amount")
-        private int grossAmount;
-
-        public void setOrderId(String orderId) {
-            this.orderId = orderId;
-        }
-
-        public void setGrossAmount(int grossAmount) {
-            this.grossAmount = grossAmount;
-        }
-
-        public String getOrderId() {
-            return orderId;
-        }
-
-        public int getGrossAmount() {
-            return grossAmount;
-        }
+    public List<ItemDetails> getItem_details() {
+        return item_details;
     }
+
+
 
     public static class CstoreEntity {
         private String store;
@@ -97,4 +113,6 @@ public class IndomaretRequestModel {
             return message;
         }
     }
+
+
 }
