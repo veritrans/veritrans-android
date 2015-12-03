ba5fe8b62f435a3c08e9eb7364d99a47eda0c04b
AnkitPachouri <ankit@leftshift.io>
2015-12-03 16:48:15 +0530

    VER-82 #Time 1h 25m BMM Application installed or not check and if not installed redirecting to play store

--- src/main/java/id/co/veritrans/sdk/fragments/InstructionBBMMoneyFragment.java
+++ src/main/java/id/co/veritrans/sdk/fragments/InstructionBBMMoneyFragment.java
@@ -1,18 +1,33 @@
 package id.co.veritrans.sdk.fragments;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.List;
 
 import id.co.veritrans.sdk.R;
+import id.co.veritrans.sdk.core.Logger;
 
 /**
  * Created by Ankit on 12/03/15.
  */
-public class InstructionBBMMoneyFragment extends Fragment {
+public class InstructionBBMMoneyFragment extends Fragment implements View.OnClickListener {
+
+    private static final String BBM_MONEY_PACKAGE = "com.monitise.client.android.bbmmoney";
+    private static final String MARKET_URL = "market://details?id=";
+    private static final String PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=";
+    private LinearLayout layoutGetBBMMoneyApp = null;
 
     @Nullable
     @Override
@@ -20,6 +35,46 @@ public class InstructionBBMMoneyFragment extends Fragment {
             savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_instruction_bbm_money, container, false);
+        initialiseView(view);
         return view;
+    }
+
+    private void initialiseView(View view){
+        layoutGetBBMMoneyApp = (LinearLayout) view.findViewById(R.id.layout_get_bbm__money_app);
+        if (isBBMMoneyInstalled(BBM_MONEY_PACKAGE)){
+            layoutGetBBMMoneyApp.setVisibility(View.GONE);
+        } else {
+            layoutGetBBMMoneyApp.setVisibility(View.VISIBLE);
+        }
+
+        layoutGetBBMMoneyApp.setOnClickListener(this);
+    }
+
+    private boolean isBBMMoneyInstalled(String bbmUri) {
+        boolean isInstalled = false;
+        PackageManager pm = getActivity().getPackageManager();
+        try {
+            pm.getPackageInfo(bbmUri, PackageManager.GET_ACTIVITIES);
+            isInstalled = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            isInstalled = false;
+        }
+
+        return isInstalled;
+    }
+
+    private void openPlayStore() {
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(MARKET_URL + BBM_MONEY_PACKAGE)));
+        } catch (ActivityNotFoundException activityNotFoundException) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PLAY_STORE_URL + BBM_MONEY_PACKAGE)));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.layout_get_bbm__money_app) {
+            openPlayStore();
+        }
     }
 }
