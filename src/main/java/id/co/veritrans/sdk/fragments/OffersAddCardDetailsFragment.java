6fec36ed894635b7aea0a3650f760a3ddf7f00cb
AnkitPachouri <ankit@leftshift.io>
2015-12-11 15:40:45 +0530

    VER-101 #Time 1h Show or hide of pay with instalment and offer applied or not layouts on Add Card detail

--- src/main/java/id/co/veritrans/sdk/fragments/OffersAddCardDetailsFragment.java
+++ src/main/java/id/co/veritrans/sdk/fragments/OffersAddCardDetailsFragment.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
@@ -29,13 +31,17 @@ public class OffersAddCardDetailsFragment extends Fragment {
     private TextViewFont textViewTitleCardDetails = null;
     private TextViewFont textViewOfferName = null;
 
-    private TextViewFont textViewOfferStatus = null;
+    private TextViewFont textViewOfferApplied = null;
+    private TextViewFont textViewOfferNotApplied = null;
+    private LinearLayout layoutOfferStatus = null;
     private Button buttonPayNow = null;
     private static final String OFFER_NAME = "offer_name";
     private String offerName = null;
     private ImageView imageViewPlus = null;
     private ImageView imageViewMinus = null;
     private TextViewFont textViewInstalment = null;
+    private RelativeLayout layoutPayWithInstalment = null;
+
 
 
     public static OffersAddCardDetailsFragment newInstance(String offerName) {
@@ -69,13 +75,23 @@ public class OffersAddCardDetailsFragment extends Fragment {
             textViewTitleCardDetails = (TextViewFont) getActivity().findViewById(R.id
                     .text_title_card_details);
             textViewOfferName = (TextViewFont) getActivity().findViewById(R.id.text_title_offer_name);
+
             setToolbar();
 
-            textViewOfferStatus = (TextViewFont) view.findViewById(R.id.text_offer_status);
+            textViewOfferApplied = (TextViewFont) view.findViewById(R.id.text_offer_status_applied);
+            textViewOfferNotApplied = (TextViewFont) view.findViewById(R.id
+                    .text_offer_status_not_applied);
+            layoutOfferStatus = (LinearLayout) view.findViewById(R.id.layout_offer_status);
             buttonPayNow = (Button) view.findViewById(R.id.btn_pay_now);
+
             imageViewPlus = (ImageView) view.findViewById(R.id.img_plus);
             imageViewMinus = (ImageView) view.findViewById(R.id.img_minus);
             textViewInstalment = (TextViewFont) view.findViewById(R.id.text_instalment);
+            layoutPayWithInstalment = (RelativeLayout) view.findViewById(R.id
+                    .layout_pay_with_instalments);
+
+            hideOrShowPayWithInstalment(false);
+            hideOrShowOfferStatus(false, false);
         }
     }
 
@@ -84,5 +100,28 @@ public class OffersAddCardDetailsFragment extends Fragment {
         textViewTitleCardDetails.setVisibility(View.VISIBLE);
         textViewOfferName.setVisibility(View.VISIBLE);
         textViewOfferName.setText(offerName);
+    }
+
+    private void hideOrShowPayWithInstalment(boolean isShowLayout){
+        if (isShowLayout){
+            layoutPayWithInstalment.setVisibility(View.VISIBLE);
+        } else {
+            layoutPayWithInstalment.setVisibility(View.GONE);
+        }
+    }
+
+    private void hideOrShowOfferStatus(boolean isShowLayout, boolean isOfferApplied){
+        if (isShowLayout){
+            layoutOfferStatus.setVisibility(View.VISIBLE);
+            if (isOfferApplied){
+                textViewOfferApplied.setVisibility(View.VISIBLE);
+                textViewOfferNotApplied.setVisibility(View.GONE);
+            } else {
+                textViewOfferApplied.setVisibility(View.GONE);
+                textViewOfferNotApplied.setVisibility(View.VISIBLE);
+            }
+        } else {
+            layoutOfferStatus.setVisibility(View.GONE);
+        }
     }
 }
