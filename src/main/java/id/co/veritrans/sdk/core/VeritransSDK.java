db73d02b20746a95d5967f20e87aaad04e83f70c
rakawm <westumogandhi@gmail.com>
2016-02-17 09:57:41 +0700

    Add flurry analytics.
    1. Add flurry library
    2. Initialize flurry on SDK initialization
    3. Add proguard rules

--- src/main/java/id/co/veritrans/sdk/core/VeritransSDK.java
+++ src/main/java/id/co/veritrans/sdk/core/VeritransSDK.java
@@ -8,6 +8,8 @@ import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.text.TextUtils;
 
+import com.flurry.android.FlurryAgent;
+
 import java.util.ArrayList;
 
 import id.co.veritrans.sdk.R;
@@ -75,6 +77,7 @@ public class VeritransSDK {
             merchantServerUrl = veritransBuilder.merchantServerUrl;
             initializeFonts();
             initializeSharedPreferences();
+            initializeFlurry();
             return veritransSDK;
         } else {
             return null;
@@ -94,6 +97,10 @@ public class VeritransSDK {
 
     private static void initializeSharedPreferences() {
         mPreferences = context.getSharedPreferences(LOCAL_DATA_PREFERENCES, Context.MODE_PRIVATE);
+    }
+
+    private static void initializeFlurry() {
+        FlurryAgent.init(context, context.getString(R.string.flurry_api_key));
     }
 
     /**
