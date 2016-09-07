9f6ca570e1055547c89a0502bcff03402ded9fbc
ziahaqi <zia.ulhack@gmail.com>
2016-09-07 14:40:56 +0700

    add midtrans project

--- src/main/java/id/co/veritrans/sdk/callbacks/AnyOfferClickedListener.java
+++
@@ -1,8 +0,0 @@
-package id.co.veritrans.sdk.callbacks;
-
-/**
- * Created by Ankit on 12/9/15.
- */
-public interface AnyOfferClickedListener {
-    void onOfferClicked(int position, String offerName, String offerType);
-}
--- src/main/java/id/co/veritrans/sdk/callbacks/PaymentStatusCallback.java
+++
@@ -1,9 +0,0 @@
-package id.co.veritrans.sdk.callbacks;
-
-import id.co.veritrans.sdk.models.TransactionStatusResponse;
-
-public interface PaymentStatusCallback {
-    public void onFailure(String errorMessage, TransactionStatusResponse transactionStatusResponse);
-
-    public void onSuccess(TransactionStatusResponse transactionStatusResponse);
-}
