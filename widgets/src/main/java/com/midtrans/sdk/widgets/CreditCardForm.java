7a2bb50d94ace6291ceff0ade9c7c9d1dc3a7ded
ziahaqi <zia.ulhack@gmail.com>
2016-09-15 15:33:39 +0700

    update app name to veritans

--- widgets/src/main/java/com/midtrans/sdk/widgets/CreditCardForm.java
+++ widgets/src/main/java/com/midtrans/sdk/widgets/CreditCardForm.java
@@ -16,7 +16,6 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
