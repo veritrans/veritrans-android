b5e45ac8c4a1f2ae8611dea743eb1f64f8494caf
rakawestu <rakamogandhi@hotmail.com>
2016-12-21 15:50:41 +0700

    Error return failed, invalid is for unknown error

--- uikit/src/main/java/com/midtrans/sdk/uikit/activities/PaymentMethodsActivity.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/activities/PaymentMethodsActivity.java
@@ -573,10 +573,8 @@ public class PaymentMethodsActivity extends BaseActivity implements PaymentMetho
                         midtransSDK.notifyTransactionFinished(new TransactionResult(response, null, TransactionResult.STATUS_SUCCESS));
                     } else if (response.getStatusCode().equals(getString(R.string.success_code_201))) {
                         midtransSDK.notifyTransactionFinished(new TransactionResult(response, null, TransactionResult.STATUS_PENDING));
-                    } else if (response.getStatusCode().equals(getString(R.string.failed_code_400))) {
-                        midtransSDK.notifyTransactionFinished(new TransactionResult(response, null, TransactionResult.STATUS_FAILED));
                     } else {
-                        midtransSDK.notifyTransactionFinished(new TransactionResult(response, null, TransactionResult.STATUS_INVALID));
+                        midtransSDK.notifyTransactionFinished(new TransactionResult(response, null, TransactionResult.STATUS_FAILED));
                     }
                 } else {
                     midtransSDK.notifyTransactionFinished(new TransactionResult(null, null, TransactionResult.STATUS_INVALID));
