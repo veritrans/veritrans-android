30d36b4fdbf6bd2d3a5dbd585f0889bb366aee9b
ziahaqi <zia.ulhack@gmail.com>
2016-12-27 11:53:58 +0700

    fix bug double saved card, in one click payment

--- uikit/src/main/java/com/midtrans/sdk/uikit/activities/CreditDebitCardFlowActivity.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/activities/CreditDebitCardFlowActivity.java
@@ -799,8 +799,10 @@ public class CreditDebitCardFlowActivity extends BaseActivity implements ReadBan
             } else {
                 //if token storage on merchantserver then saved cards can be used just for two click
                 String clickType = midtransSDK.getTransactionRequest().getCardClickType();
-                if (!TextUtils.isEmpty(clickType) && clickType.equals(getString(R.string.card_click_type_two_click))) {
-                    filteredCards.addAll(cards);
+                for (SaveCardRequest card : cards) {
+                    if (clickType.equals(getString(R.string.card_click_type_two_click)) && card.getType().equals(getString(R.string.card_click_type_two_click))) {
+                        filteredCards.add(card);
+                    }
                 }
             }
         }
