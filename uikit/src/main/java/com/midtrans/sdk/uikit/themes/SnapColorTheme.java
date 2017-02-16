8e9e6ecf4d3c4ba1cac6272d454137056711626e
rakawestu <rakamogandhi@hotmail.com>
2017-02-16 09:31:19 +0700

    Add class for customizing color theme

---
+++ uikit/src/main/java/com/midtrans/sdk/uikit/themes/SnapColorTheme.java
@@ -0,0 +1,13 @@
+package com.midtrans.sdk.uikit.themes;
+
+/**
+ * Created by rakawm on 2/16/17.
+ */
+
+public interface SnapColorTheme {
+    int getPrimaryColor(String themeName);
+
+    int getPrimaryDarkColor(String themeName);
+
+    int getSecondaryColor(String themeName);
+}
