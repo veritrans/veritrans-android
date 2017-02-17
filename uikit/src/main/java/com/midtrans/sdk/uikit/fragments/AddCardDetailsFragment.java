d3680919395d895df5d7428e9b20abae63467301
ziahaqi <zia.ulhack@gmail.com>
2017-02-17 15:39:17 +0700

    - fix bug show icon on savedcardlayout

--- uikit/src/main/java/com/midtrans/sdk/uikit/fragments/AddCardDetailsFragment.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/fragments/AddCardDetailsFragment.java
@@ -174,6 +174,7 @@ public class AddCardDetailsFragment extends Fragment {
 
     @Override
     public void onDestroyView() {
+        ((CreditDebitCardFlowActivity)getActivity()).showDeleteCardIcon(false);
         super.onDestroyView();
     }
 
