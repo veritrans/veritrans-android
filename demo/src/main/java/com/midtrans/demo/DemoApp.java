3394d2991a66c01547a5f7839dbb9084067d1d5a
rakawestu <rakamogandhi@hotmail.com>
2017-03-15 14:38:00 +0700

    Update demo app main screen

---
+++ demo/src/main/java/com/midtrans/demo/DemoApp.java
@@ -0,0 +1,14 @@
+package com.midtrans.demo;
+
+import android.app.Application;
+
+/**
+ * Created by rakawm on 3/15/17.
+ */
+
+public class DemoApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
