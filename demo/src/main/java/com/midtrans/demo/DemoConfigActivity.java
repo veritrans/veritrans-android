1cfeecdfbb9c60fcaaba7e7e075a73c14b60b225
rakawestu <rakamogandhi@hotmail.com>
2017-03-15 08:48:52 +0700

    Add demo app module

---
+++ demo/src/main/java/com/midtrans/demo/DemoConfigActivity.java
@@ -0,0 +1,17 @@
+package com.midtrans.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rakawm on 3/15/17.
+ */
+
+public class DemoConfigActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_configuration);
+    }
+}
