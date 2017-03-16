6e2f78db333ac083fa037173b89753a57d17846b
rakawestu <rakamogandhi@hotmail.com>
2017-03-16 16:24:45 +0700

    Add crashlytics

--- demo/src/main/java/com/midtrans/demo/DemoApp.java
+++ demo/src/main/java/com/midtrans/demo/DemoApp.java
@@ -1,6 +1,8 @@
 package com.midtrans.demo;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by rakawm on 3/15/17.
@@ -10,5 +12,10 @@ public class DemoApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initCrashlytics();
+    }
+
+    private void initCrashlytics() {
+        Fabric.with(this, new Crashlytics());
     }
 }
