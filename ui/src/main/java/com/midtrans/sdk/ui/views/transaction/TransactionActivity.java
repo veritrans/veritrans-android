58dab668c73fc1653c02a4c29edd3ddb1e9b1ec0
rakawestu <rakamogandhi@hotmail.com>
2017-03-29 15:05:28 +0700

    Refine transaction (payment methods) screen

--- ui/src/main/java/com/midtrans/sdk/ui/views/transaction/TransactionActivity.java
+++ ui/src/main/java/com/midtrans/sdk/ui/views/transaction/TransactionActivity.java
@@ -1,17 +1,20 @@
 package com.midtrans.sdk.ui.views.transaction;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.MenuItem;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.midtrans.sdk.ui.MidtransUi;
 import com.midtrans.sdk.ui.R;
 import com.midtrans.sdk.ui.abtracts.BaseActivity;
 import com.midtrans.sdk.ui.adapters.ItemDetailsAdapter;
@@ -21,8 +24,11 @@ import com.midtrans.sdk.ui.constants.PaymentType;
 import com.midtrans.sdk.ui.models.PaymentMethodModel;
 import com.midtrans.sdk.ui.models.PaymentResult;
 import com.midtrans.sdk.ui.utils.Logger;
-import com.midtrans.sdk.ui.views.creditcard.CreditCardActivity;
+import com.midtrans.sdk.ui.views.creditcard.details.CreditCardDetailsActivity;
 import com.midtrans.sdk.ui.widgets.DefaultTextView;
+import com.midtrans.sdk.ui.widgets.FancyButton;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -30,17 +36,25 @@ import java.util.List;
  * Created by ziahaqi on 2/19/17.
  */
 
-public class TransactionActivity extends BaseActivity implements TransactionContract.View,
-        PaymentMethodsAdapter.PaymentMethodListener, ItemDetailsAdapter.ItemDetailListener {
+public class TransactionActivity
+        extends BaseActivity
+        implements TransactionContract.View,
+        PaymentMethodsAdapter.PaymentMethodListener,
+        ItemDetailsAdapter.ItemDetailListener {
 
     private TransactionPresenter presenter;
     private PaymentMethodsAdapter paymentMethodsAdapter;
     private ItemDetailsAdapter itemDetailsAdapter;
 
     private LinearLayout layoutProgressContainer;
-    private RecyclerView rvPaymentMethods;
-    private RecyclerView rvItemDetails;
-    private DefaultTextView tvHeaderTitle;
+    private RecyclerView paymentMethodsContainer;
+    private RecyclerView itemDetailsContainer;
+    private DefaultTextView merchantNameTitle;
+    private DefaultTextView errorMessageText;
+    private FancyButton retryButton;
+    private FancyButton cancelButton;
+    private RelativeLayout containerError;
+    private ImageView merchantLogo;
 
 
     @Override
@@ -48,21 +62,11 @@ public class TransactionActivity extends BaseActivity implements TransactionCont
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
         initProperties();
-        bindView();
+        initViews();
         initThemeColor();
-
-        setupView();
-        checkoutTransaction();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return false;
-        }
-        return super.onOptionsItemSelected(item);
+        bindViews();
+        initTransaction();
+        initRetryAndCancel();
     }
 
     private void initProperties() {
@@ -71,100 +75,137 @@ public class TransactionActivity extends BaseActivity implements TransactionCont
         itemDetailsAdapter = new ItemDetailsAdapter(this, presenter.getItemDetails());
     }
 
-    private void checkoutTransaction() {
-        showProgressContainer(true);
-        presenter.checkout();
-    }
-
-    private void setupView() {
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        rvPaymentMethods.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        rvPaymentMethods.setAdapter(paymentMethodsAdapter);
-
-        rvItemDetails.setLayoutManager(new LinearLayoutManager(this));
-        rvItemDetails.setAdapter(itemDetailsAdapter);
-
-    }
-
-    private void bindView() {
+    private void initViews() {
         toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         layoutProgressContainer = (LinearLayout) findViewById(R.id.progress_container);
-        rvPaymentMethods = (RecyclerView) findViewById(R.id.rv_payment_methods);
-        rvItemDetails = (RecyclerView) findViewById(R.id.rv_item_list);
-        tvHeaderTitle = (DefaultTextView) findViewById(R.id.title_header);
+        paymentMethodsContainer = (RecyclerView) findViewById(R.id.container_payment_methods);
+        itemDetailsContainer = (RecyclerView) findViewById(R.id.container_item_details);
+        merchantNameTitle = (DefaultTextView) findViewById(R.id.merchant_name);
+        merchantLogo = (ImageView) findViewById(R.id.merchant_logo);
+        retryButton = (FancyButton) findViewById(R.id.checkout_retry_button);
+        cancelButton = (FancyButton) findViewById(R.id.cancel_button);
+        containerError = (RelativeLayout) findViewById(R.id.container_error);
+        errorMessageText = (DefaultTextView) findViewById(R.id.error_text);
+    }
+
+    private void initTransaction() {
+        showProgressContainer(true);
+        presenter.init();
+    }
+
+    private void bindViews() {
+        setSupportActionBar(toolbar);
+
+        paymentMethodsContainer.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        paymentMethodsContainer.setAdapter(paymentMethodsAdapter);
+
+        itemDetailsContainer.setLayoutManager(new LinearLayoutManager(this));
+        itemDetailsContainer.setAdapter(itemDetailsAdapter);
     }
 
 
     @Override
-    public void showProgressContainer(boolean show) {
-        if (show) {
+    public void showMerchantNameOrLogo(final String merchantName, String merchantLogoUrl) {
+        if (merchantLogoUrl != null && !TextUtils.isEmpty(merchantLogoUrl)) {
+            merchantNameTitle.setVisibility(View.GONE);
+            merchantLogo.setVisibility(View.VISIBLE);
+            Picasso.with(this)
+                    .load(merchantLogoUrl)
+                    .into(merchantLogo, new Callback() {
+                        @Override
+                        public void onSuccess() {
+                            Logger.d(TAG, "Success to load merchant logo.");
+                        }
+
+                        @Override
+                        public void onError() {
+                            Logger.d(TAG, "Failed to load merchant logo.");
+                            merchantLogo.setVisibility(View.GONE);
+                            merchantNameTitle.setVisibility(View.VISIBLE);
+                            merchantNameTitle.setText(merchantName);
+                        }
+                    });
+        } else {
+            merchantLogo.setVisibility(View.GONE);
+            merchantNameTitle.setVisibility(View.VISIBLE);
+            merchantNameTitle.setText(merchantName);
+        }
+    }
+
+    @Override
+    public void showProgressContainer(boolean shown) {
+        if (shown) {
             layoutProgressContainer.setVisibility(View.VISIBLE);
         } else {
             layoutProgressContainer.setVisibility(View.GONE);
         }
     }
 
-    @Override
-    public void showConfirmationDialog(String errorMessage) {
-        AlertDialog alertDialog = new AlertDialog.Builder(this)
-                .setMessage(errorMessage)
-                .setPositiveButton(R.string.btn_retry, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        checkoutTransaction();
-                    }
-                })
-                .setNegativeButton(R.string.btn_cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        finish();
-                    }
-                })
-                .create();
-        alertDialog.show();
-    }
-
     /**
-     * @param enabledPayments Listof enabled payment method
+     * @param enabledPayments List of enabled payment method
      */
     @Override
-    public void showPaymentMethods(List<PaymentMethodModel> enabledPayments, String merchantName) {
+    public void showPaymentMethods(List<PaymentMethodModel> enabledPayments) {
         initTheme();
-        rvItemDetails.setBackgroundColor(presenter.getPrimaryColor());
-        tvHeaderTitle.setText(merchantName);
+        itemDetailsContainer.setBackgroundColor(presenter.getPrimaryColor());
         paymentMethodsAdapter.setData(enabledPayments);
+    }
+
+    @Override
+    public void updateColorTheme() {
+        Drawable backButton = ContextCompat.getDrawable(this, R.drawable.ic_back);
+        backButton.setColorFilter(MidtransUi.getInstance().getColorTheme().getPrimaryDarkColor(), PorterDuff.Mode.SRC_ATOP);
+        toolbar.setNavigationIcon(backButton);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.sendPaymentResult(new PaymentResult(true));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void showErrorContainer() {
+        errorMessageText.setText(getString(R.string.error_message_failed_checkout));
+        containerError.setVisibility(View.VISIBLE);
+        retryButton.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void showErrorContainer(String message, boolean needRetry) {
+        errorMessageText.setText(message);
+        containerError.setVisibility(View.VISIBLE);
+        if (needRetry) {
+            retryButton.setVisibility(View.VISIBLE);
+        } else {
+            retryButton.setVisibility(View.GONE);
+        }
+
     }
 
     @Override
     public void onItemClick(int position) {
         PaymentMethodModel paymentMethod = paymentMethodsAdapter.getItem(position);
-        Log.d(TAG, "method:" + paymentMethod.getName());
+        Logger.d(TAG, "method:" + paymentMethod.getName());
         showPaymentActivity(paymentMethod);
     }
 
     private void showPaymentActivity(PaymentMethodModel paymentMethod) {
         switch (paymentMethod.getPaymentType()) {
             case PaymentType.CREDIT_CARD:
-                Intent intent = new Intent(this, CreditCardActivity.class);
+                Intent intent = new Intent(this, CreditCardDetailsActivity.class);
                 startActivityForResult(intent, Constants.INTENT_CODE_PAYMENT);
                 break;
-
             case PaymentType.BANK_TRANSFER:
-
                 break;
             default:
-
                 break;
         }
     }
 
     @Override
     public void onItemShown() {
-
     }
 
     @Override
@@ -175,24 +216,45 @@ public class TransactionActivity extends BaseActivity implements TransactionCont
         if (requestCode == Constants.INTENT_CODE_PAYMENT) {
             if (resultCode == RESULT_OK) {
                 PaymentResult result = (PaymentResult) data.getSerializableExtra(Constants.PAYMENT_RESULT);
-                Logger.d(TAG, "onActivityResult():response:" + result);
+                Logger.d(TAG, "onActivityResult(): response:" + result);
+
                 if (result != null) {
+                    MidtransUi.getInstance().clearTransaction();
                     presenter.sendPaymentResult(result);
                     finish();
                 }
-
             } else if (resultCode == RESULT_CANCELED) {
-                PaymentResult result = (PaymentResult) data.getSerializableExtra(Constants.PAYMENT_RESULT);
-                Logger.d(TAG, "onActivityResult():response:" + result);
+                if (data != null && data.getSerializableExtra(Constants.PAYMENT_RESULT) != null) {
+                    PaymentResult result = (PaymentResult) data.getSerializableExtra(Constants.PAYMENT_RESULT);
+                    Logger.d(TAG, "onActivityResult():response:" + result);
 
-                if (result != null) {
-                    presenter.sendPaymentResult(result);
-                    finish();
+                    if (result != null) {
+                        presenter.sendPaymentResult(result);
+                        finish();
+                    }
                 }
             }
-
         } else {
             Logger.d(TAG, "failed to send result back " + requestCode);
         }
+    }
+
+    private void initRetryAndCancel() {
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                containerError.setVisibility(View.GONE);
+                showProgressContainer(true);
+                initTransaction();
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.sendPaymentResult(new PaymentResult(true));
+                finish();
+            }
+        });
     }
 }
