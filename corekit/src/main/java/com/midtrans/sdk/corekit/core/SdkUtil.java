5d2687a93b2f4122305714021a93930976173325
Fajar Siddiq <muhammad.siddiq@midtrans.com>
2018-03-19 19:11:42 +0700

    Updated SecurePrefs version; handled preferences getInt returns ClassCastException;

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/SdkUtil.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/SdkUtil.java
@@ -696,7 +696,12 @@ public class SdkUtil {
     public static SecurePreferences newPreferences(Context context, String name) {
 
         SecurePreferences preferences = new SecurePreferences(context, context.getString(R.string.PREFERENCE_PASSWORD), name);
-        int prefVersion = preferences.getInt(Constants.KEY_PREFERENCES_VERSION, 0);
+        int prefVersion;
+        try {
+            prefVersion = preferences.getInt(Constants.KEY_PREFERENCES_VERSION, 0);
+        } catch (ClassCastException e) {
+            prefVersion = 0;
+        }
         if (prefVersion == 0 || prefVersion < Constants.PREFERENCES_VERSION) {
             SecurePreferences.Editor editor = preferences.edit();
             editor.clear();
