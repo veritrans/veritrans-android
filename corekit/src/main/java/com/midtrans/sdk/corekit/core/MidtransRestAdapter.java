a46e0d294594da771683686360114f817f14ab1a
ziahq <zia.ulhaq@midtrans.com>
2018-06-07 14:46:56 +0700

    add tls compat

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/MidtransRestAdapter.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/MidtransRestAdapter.java
@@ -1,5 +1,7 @@
 package com.midtrans.sdk.corekit.core;
 
+import android.os.Build;
+
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -9,13 +11,26 @@ import com.midtrans.sdk.corekit.models.snap.params.CreditCardPaymentParams;
 import com.midtrans.sdk.corekit.utilities.CustomTypeAdapter;
 
 import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.ConnectionSpec;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.TlsVersion;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -82,13 +97,11 @@ public class MidtransRestAdapter {
     }
 
     private static Gson newGson() {
-        Gson gson = new GsonBuilder()
+        return new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.IDENTITY)
                 .registerTypeAdapter(Date.class, new DateTypeAdapter())
                 .registerTypeAdapter(CreditCardPaymentParams.class, new CustomTypeAdapter())
                 .create();
-
-        return gson;
     }
 
     private static Interceptor newHeaderInterceptor() {
@@ -123,19 +136,62 @@ public class MidtransRestAdapter {
         };
     }
 
+    private static OkHttpClient.Builder delegateTlsCompat(OkHttpClient.Builder builder) {
+        if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 22) {
+            try {
+
+                SSLContext sc = SSLContext.getInstance("TLSv1.2");
+                sc.init(null, null, null);
+                builder.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()), getTrustManager());
+
+                ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                        .tlsVersions(TlsVersion.TLS_1_2)
+                        .build();
+
+                List<ConnectionSpec> specs = new ArrayList<>();
+                specs.add(cs);
+                specs.add(ConnectionSpec.COMPATIBLE_TLS);
+                specs.add(ConnectionSpec.CLEARTEXT);
+
+                builder.connectionSpecs(specs);
+            } catch (Exception exc) {
+                Logger.e("OkHttpTLSCompat Error while setting TLS 1.2", exc);
+            }
+        }
+
+        return builder;
+    }
+
 
     private static OkHttpClient newSnapOkHttpClient(int timeout) {
-        return new OkHttpClient.Builder()
-                .addInterceptor(newHttpLoggingInterceptor())
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        delegateTlsCompat(builder);
+        builder.addInterceptor(newHttpLoggingInterceptor())
                 .addInterceptor(newSnapHeaderInterceptor())
                 .connectTimeout(timeout, TimeUnit.SECONDS)
                 .readTimeout(timeout, TimeUnit.SECONDS)
-                .writeTimeout(timeout, TimeUnit.SECONDS)
-                .build();
+                .writeTimeout(timeout, TimeUnit.SECONDS);
+
+        return builder.build();
+    }
+
+    private static X509TrustManager getTrustManager() throws NoSuchAlgorithmException, KeyStoreException {
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init((KeyStore) null);
+        TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+        if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+            throw new IllegalStateException("Unexpected default trust managers:"
+                    + Arrays.toString(trustManagers));
+        }
+
+        return (X509TrustManager) trustManagers[0];
     }
 
     private static OkHttpClient newOkHttpClient(int timeout) {
-        return new OkHttpClient.Builder()
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        delegateTlsCompat(builder);
+        return builder
                 .addInterceptor(newHttpLoggingInterceptor())
                 .addInterceptor(newHeaderInterceptor())
                 .connectTimeout(timeout, TimeUnit.SECONDS)
