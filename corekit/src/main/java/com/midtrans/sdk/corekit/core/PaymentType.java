011c38de58352e0afa3760277fb5033a50b91206
farhanpahlevi <farhan.pahlevi@midtrans.com>
2018-10-23 13:19:17 +0700

    Add akulaku in PaymentType

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/PaymentType.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/PaymentType.java
@@ -35,5 +35,6 @@ public class PaymentType {
     public static final String GOPAY = "gopay";
     public static final String DANAMON_ONLINE = "danamon_online";
     public static final String INDOMARET_CSTORE = "cstore";
+    public static final String AKULAKU = "akulaku";
 
 }
