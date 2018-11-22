8dfa876536c093c7067b08ec2f167d0827a4f97b
farhanpahlevi <farhan.pahlevi@midtrans.com>
2018-11-22 17:26:57 +0700

    Make transaction request with builder

--- demo/src/main/java/com/midtrans/demo/MainActivity.java
+++ demo/src/main/java/com/midtrans/demo/MainActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.midtrans.sdk.corekit.base.enums.Environment;
-import com.midtrans.sdk.corekit.core.MidtransCoreSdk;
+import com.midtrans.sdk.corekit.base.model.BankType;
+import com.midtrans.sdk.corekit.core.MidtransSdk;
 import com.midtrans.sdk.corekit.core.merchant.model.checkout.CheckoutCallback;
 import com.midtrans.sdk.corekit.core.merchant.model.checkout.request.TransactionRequest;
-import com.midtrans.sdk.corekit.core.merchant.model.checkout.request.optional.GopayDeeplink;
+import com.midtrans.sdk.corekit.core.merchant.model.checkout.request.optional.customer.BillingAddress;
+import com.midtrans.sdk.corekit.core.merchant.model.checkout.request.optional.customer.CustomerDetails;
+import com.midtrans.sdk.corekit.core.merchant.model.checkout.request.optional.customer.ShippingAddress;
+import com.midtrans.sdk.corekit.core.merchant.model.checkout.request.specific.creditcard.CreditCard;
 import com.midtrans.sdk.corekit.core.merchant.model.checkout.response.TokenResponse;
 import com.midtrans.sdk.corekit.core.snap.model.transaction.TransactionOptionsCallback;
 import com.midtrans.sdk.corekit.core.snap.model.transaction.response.TransactionOptionsResponse;
+import com.midtrans.sdk.corekit.utilities.Currency;
 import com.midtrans.sdk.corekit.utilities.Logger;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,21 +25,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MidtransCoreSdk
-                .init(this,
+        MidtransSdk
+                .builder(this,
                         BuildConfig.CLIENT_KEY,
                         BuildConfig.BASE_URL)
                 .setLogEnabled(true)
                 .setEnvironment(Environment.SANDBOX)
                 .build();
 
+        TransactionRequest trxRequest = TransactionRequest
+                .builder("sample_sdk_test_core_00005", 20000.0)
+                .setCurrency(Currency.IDR)
+                .setGopayCallbackDeepLink("demo://midtrans")
+                .setCreditCard(CreditCard
+                        .normalClickBuilder(false, CreditCard.AUTHENTICATION_TYPE_NONE)
+                        .setTokenId("")
+                        .setSaveCard(true)
+                        .setBank(BankType.BCA)
+                        .setInstallment(false, null)
+                        .setBlackListBins(null)
+                        .setWhiteListBins(null)
+                        .setSavedTokens(null)
+                        .setChannel(CreditCard.MIGS)
+                        .build())
+                .setCustomerDetails(CustomerDetails
+                        .builder("FirstName",
+                                "LastName",
+                                "email@mail.com",
+                                "6281234567890")
+                        .setBillingAddress(new BillingAddress("Firstname",
+                                "LastName",
+                                "mail@mail.com",
+                                "Bogor",
+                                "16710",
+                                "62877",
+                                "id"))
+                        .setShippingAddress(new ShippingAddress("Firstname",
+                                "LastName",
+                                "mail@mail.com",
+                                "Bogor",
+                                "16710",
+                                "62877",
+                                "id"))
+                        .build())
+                .build();
 
-        GopayDeeplink gopayDeeplink = new GopayDeeplink("gopay");
-        TransactionRequest transactionRequest = new TransactionRequest("sample_sdk_test_core_00005", 20000);
-        transactionRequest.setGopay(gopayDeeplink);
+        TransactionRequest trxInstance = TransactionRequest.getInstance();
 
-        MidtransCoreSdk.getInstance().setTransactionRequest(transactionRequest);
-        MidtransCoreSdk.getInstance().checkout(new CheckoutCallback() {
+        MidtransSdk.getInstance().setTransactionRequest(trxRequest);
+        MidtransSdk.getInstance().checkout(new CheckoutCallback() {
             @Override
             public void onSuccess(TokenResponse token) {
                 Logger.debug("MIDTRANS SDK NEW RETURN SUCCESS >>> " + token.getSnapToken());
@@ -54,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getTransactionOptions(String snapToken) {
-        MidtransCoreSdk.getInstance().getTransactionOptions(snapToken, new TransactionOptionsCallback() {
+        MidtransSdk.getInstance().getPaymentInfo(snapToken, new TransactionOptionsCallback() {
             @Override
             public void onSuccess(TransactionOptionsResponse transaction) {
                 Logger.debug("MIDTRANS SDK NEW RETURN SUCCESS >>> " + transaction.getToken());
