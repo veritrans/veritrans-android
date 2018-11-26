9d609eeae7b6e9a3f20a1ecd0b465fdbb7615967
farhanpahlevi <farhan.pahlevi@midtrans.com>
2018-11-26 12:38:07 +0700

    Add PaymentType constants

---
+++ corekit/src/main/java/com/midtrans/sdk/corekit/base/model/PaymentType.java
@@ -0,0 +1,30 @@
+package com.midtrans.sdk.corekit.base.model;
+
+public class PaymentType {
+    public static String CREDIT_CARD = "credit_card";
+
+    public static String BNI_VA = "bni_va";
+    public static String BCA_VA = "bca_va";
+    public static String BANK_TRANSFER = "bank_transfer";
+    public static String PERMATA_VA = "permata_va";
+    public static String E_CHANNEL = "echannel";
+    public static String OTHER_VA = "other_va";
+
+    public static String GOPAY = "gopay";
+
+    public static String KLIK_BCA = "bca_klikbca";
+    public static String BCA_KLIKPAY = "bca_klikpay";
+    public static String CIMB_CLICKS = "cimb_clicks";
+    public static String MANDIRI_CLICKPAY = "mandiri_clickpay";
+    public static String BRI_EPAY = "bri_epay";
+    public static String DANAMON_ONLINE = "danamon_online";
+    public static String MANDIRI_ECASH = "mandiri_ecash";
+
+    public static String TELKOMSEL_CASH = "telkomsel_cash";
+    public static String XL_TUNAI = "xl_tunai";
+    public static String INDOSAT_DOMPETKU = "indosat_dompetku";
+
+    public static String CSTORE = "cstore";
+    public static String KIOSON = "kioson";
+    public static String INDOMARET = "indomaret";
+}
