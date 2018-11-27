547100b49de9c9c049585fd0551570c08e2bad1c
farhanpahlevi <farhan.pahlevi@midtrans.com>
2018-11-27 14:47:16 +0700

    Add KlikBca Payment request dan response

---
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/snap/model/pay/request/klikbca/KlikBcaPaymentParams.java
@@ -0,0 +1,16 @@
+package com.midtrans.sdk.corekit.core.snap.model.pay.request.klikbca;
+
+import com.google.gson.annotations.SerializedName;
+
+public class KlikBcaPaymentParams {
+    @SerializedName("user_id")
+    private String userId;
+
+    public KlikBcaPaymentParams(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+}
