3e66800080e9a7139f6171badfe6d1a403509282
suryagsaputra <suryagsaputra@gmail.com>
2018-12-03 15:17:54 +0700

    grouping payment OnlineDebit from MidtransSDK

---
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/grouppayment/MidtransOnlineDebit.java
@@ -0,0 +1,48 @@
+package com.midtrans.sdk.corekit.core.grouppayment;
+
+import com.midtrans.sdk.corekit.base.callback.MidtransCallback;
+import com.midtrans.sdk.corekit.core.snap.model.pay.response.BasePaymentResponse;
+import com.midtrans.sdk.corekit.core.snap.model.pay.response.bcaklikpay.BcaClickPayPaymentResponse;
+import com.midtrans.sdk.corekit.core.snap.model.pay.response.epaybri.BriEpayPaymentResponse;
+import com.midtrans.sdk.corekit.utilities.Validation;
+
+public class MidtransOnlineDebit extends BaseMidtransGroupPayments {
+    /**
+     * Start payment using bank transfer and va with CIMB Clicks.
+     *
+     * @param snapToken token after making checkoutWithTransaction.
+     * @param callback  for receiving callback from request.
+     */
+    public static void paymentUsingCimbClicks(final String snapToken,
+                                              final MidtransCallback<BasePaymentResponse> callback) {
+        if (Validation.isValidForNetworkCall(getSdkContext(), callback)) {
+            getSnapApiManager().paymentUsingCimbClick(snapToken, callback);
+        }
+    }
+
+    /**
+     * Start payment using bank transfer and va with CIMB Clicks.
+     *
+     * @param snapToken token after making checkoutWithTransaction.
+     * @param callback  for receiving callback from request.
+     */
+    public static void paymentUsingBcaClickPay(final String snapToken,
+                                               final MidtransCallback<BcaClickPayPaymentResponse> callback) {
+        if (Validation.isValidForNetworkCall(getSdkContext(), callback)) {
+            getSnapApiManager().paymentUsingBcaClickPay(snapToken, callback);
+        }
+    }
+
+    /**
+     * Start payment using bank transfer and va with BRI Epay.
+     *
+     * @param snapToken token after making checkoutWithTransaction.
+     * @param callback  for receiving callback from request.
+     */
+    public static void paymentUsingBriEpay(final String snapToken,
+                                           final MidtransCallback<BriEpayPaymentResponse> callback) {
+        if (Validation.isValidForNetworkCall(getSdkContext(), callback)) {
+            getSnapApiManager().paymentUsingBriEpay(snapToken, callback);
+        }
+    }
+}
