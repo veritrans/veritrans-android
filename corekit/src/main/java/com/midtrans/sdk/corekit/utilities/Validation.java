34062b75fd1aa79a971093de80764778d6a7b50d
suryagsaputra <suryagsaputra@gmail.com>
2018-12-03 16:29:01 +0700

    adding null context validation

--- corekit/src/main/java/com/midtrans/sdk/corekit/utilities/Validation.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/utilities/Validation.java
@@ -4,14 +4,20 @@ import android.content.Context;
 
 import com.midtrans.sdk.corekit.base.callback.MidtransCallback;
 
-import static com.midtrans.sdk.corekit.utilities.Constants.TAG;
-
 public class Validation {
+
+    private static final String TAG = "Validation";
+
     public static <T> Boolean isValidForNetworkCall(Context context, MidtransCallback<T> callback) {
+        if (context == null) {
+            Logger.error(TAG, Constants.MESSAGE_ERROR_MISSING_CONTEXT);
+        }
+
         if (callback == null) {
             Logger.error(TAG, Constants.MESSAGE_ERROR_CALLBACK_UNIMPLEMENTED);
             return false;
         }
+
         if (!isNetworkAvailable(context)) {
             callback.onFailed(new Throwable(Constants.MESSAGE_ERROR_FAILED_TO_CONNECT_TO_SERVER));
             return false;
