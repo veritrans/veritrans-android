e23a6adedde237669f4aa32c902cd9d9a20afcaa
suryagsaputra <suryagsaputra@gmail.com>
2018-12-03 15:18:43 +0700

    grouping payment Store from MidtransSDK

---
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/grouppayment/MidtransStore.java
@@ -0,0 +1,21 @@
+package com.midtrans.sdk.corekit.core.grouppayment;
+
+import com.midtrans.sdk.corekit.base.callback.MidtransCallback;
+import com.midtrans.sdk.corekit.core.snap.model.pay.response.BasePaymentResponse;
+import com.midtrans.sdk.corekit.utilities.Validation;
+
+public class MidtransStore extends BaseMidtransGroupPayments {
+
+    /**
+     * Start payment using bank transfer and va with Indomaret.
+     *
+     * @param snapToken token after making checkoutWithTransaction.
+     * @param callback  for receiving callback from request.
+     */
+    public static void paymentUsingIndomaret(final String snapToken,
+                                             final MidtransCallback<BasePaymentResponse> callback) {
+        if (Validation.isValidForNetworkCall(getSdkContext(), callback)) {
+            getSnapApiManager().paymentUsingIndomaret(snapToken, callback);
+        }
+    }
+}
