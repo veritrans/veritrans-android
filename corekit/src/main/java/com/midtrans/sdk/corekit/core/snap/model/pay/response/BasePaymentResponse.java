cbc5ff15bc60448a9d77b2fd228f987a7c02665b
suryagsaputra <suryagsaputra@gmail.com>
2018-12-06 12:32:05 +0700

    split property with method

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/snap/model/pay/response/BasePaymentResponse.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/snap/model/pay/response/BasePaymentResponse.java
@@ -6,63 +6,40 @@ import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 public class BasePaymentResponse implements Serializable {
+
     @SerializedName("status_code")
     @Expose
-    private String statusCode;
+    protected String statusCode;
     @SerializedName("status_message")
     @Expose
-    private String statusMessage;
+    protected String statusMessage;
     @SerializedName("redirect_url")
     @Expose
-    private String redirectUrl;
+    protected String redirectUrl;
     @SerializedName("transaction_id")
     @Expose
-    private String transactionId;
+    protected String transactionId;
     @SerializedName("order_id")
     @Expose
-    private String orderId;
+    protected String orderId;
     @SerializedName("gross_amount")
     @Expose
-    private String grossAmount;
+    protected String grossAmount;
     @SerializedName("currency")
     @Expose
-    private String currency;
+    protected String currency;
     @SerializedName("payment_type")
     @Expose
-    private String paymentType;
+    protected String paymentType;
     @SerializedName("transaction_time")
     @Expose
-    private String transactionTime;
+    protected String transactionTime;
     @SerializedName("transaction_status")
     @Expose
-    private String transactionStatus;
+    protected String transactionStatus;
     @SerializedName("finish_redirect_url")
     @Expose
-    private String finishRedirectUrl;
-
-    public String getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(String statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public String getStatusMessage() {
-        return statusMessage;
-    }
-
-    public void setStatusMessage(String statusMessage) {
-        this.statusMessage = statusMessage;
-    }
-
-    public String getRedirectUrl() {
-        return redirectUrl;
-    }
-
-    public void setRedirectUrl(String redirectUrl) {
-        this.redirectUrl = redirectUrl;
-    }
+    protected String finishRedirectUrl;
 
     public String getTransactionId() {
         return transactionId;
@@ -72,59 +49,4 @@ public class BasePaymentResponse implements Serializable {
         this.transactionId = transactionId;
     }
 
-    public String getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getGrossAmount() {
-        return grossAmount;
-    }
-
-    public void setGrossAmount(String grossAmount) {
-        this.grossAmount = grossAmount;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public String getPaymentType() {
-        return paymentType;
-    }
-
-    public void setPaymentType(String paymentType) {
-        this.paymentType = paymentType;
-    }
-
-    public String getTransactionTime() {
-        return transactionTime;
-    }
-
-    public void setTransactionTime(String transactionTime) {
-        this.transactionTime = transactionTime;
-    }
-
-    public String getTransactionStatus() {
-        return transactionStatus;
-    }
-
-    public void setTransactionStatus(String transactionStatus) {
-        this.transactionStatus = transactionStatus;
-    }
-
-    public String getFinishRedirectUrl() {
-        return finishRedirectUrl;
-    }
-
-    public void setFinishRedirectUrl(String finishRedirectUrl) {
-        this.finishRedirectUrl = finishRedirectUrl;
-    }
 }
---
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/snap/model/pay/response/BasePaymentResponseMethod.java
@@ -0,0 +1,100 @@
+package com.midtrans.sdk.corekit.core.snap.model.pay.response;
+
+public class BasePaymentResponseMethod extends BasePaymentResponse {
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+    public String getRedirectUrl() {
+        return redirectUrl;
+    }
+
+    public void setRedirectUrl(String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getGrossAmount() {
+        return grossAmount;
+    }
+
+    public void setGrossAmount(String grossAmount) {
+        this.grossAmount = grossAmount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public String getTransactionTime() {
+        return transactionTime;
+    }
+
+    public void setTransactionTime(String transactionTime) {
+        this.transactionTime = transactionTime;
+    }
+
+    public String getTransactionStatus() {
+        return transactionStatus;
+    }
+
+    public void setTransactionStatus(String transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+
+    public String getFinishRedirectUrl() {
+        return finishRedirectUrl;
+    }
+
+    public void setFinishRedirectUrl(String finishRedirectUrl) {
+        this.finishRedirectUrl = finishRedirectUrl;
+    }
+
+    public String getApprovalCode() {
+        return approvalCode;
+    }
+
+    public void setApprovalCode(String approvalCode) {
+        this.approvalCode = approvalCode;
+    }
+}
