e38216deef504b1fd5221cd4db744e66e7b050f4
surya <suryagsaputra@gmail.com>
2019-01-03 08:36:44 +0700

    extend HttpRequestCallback

--- corekit/src/main/java/com/midtrans/sdk/corekit/base/callback/MidtransCallback.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/base/callback/MidtransCallback.java
@@ -1,6 +1,6 @@
 package com.midtrans.sdk.corekit.base.callback;
 
-public interface MidtransCallback<T> {
+public interface MidtransCallback<T> extends HttpRequestCallback{
     void onSuccess(T data);
 
     void onFailed(Throwable throwable);
