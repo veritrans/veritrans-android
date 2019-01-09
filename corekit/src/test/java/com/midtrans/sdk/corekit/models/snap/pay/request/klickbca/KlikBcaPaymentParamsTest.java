c0c59f8b9df707e7363c001a235dd9d4edd676e8
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-09 16:06:52 +0700

    Add KlikBca test

---
+++ corekit/src/test/java/com/midtrans/sdk/corekit/models/snap/pay/request/klickbca/KlikBcaPaymentParamsTest.java
@@ -0,0 +1,37 @@
+package com.midtrans.sdk.corekit.models.snap.pay.request.klickbca;
+
+import com.midtrans.sdk.corekit.core.api.snap.model.pay.request.gopay.GopayPaymentParams;
+import com.midtrans.sdk.corekit.core.api.snap.model.pay.request.klikbca.KlikBcaPaymentParams;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+@RunWith(PowerMockRunner.class)
+public class KlikBcaPaymentParamsTest {
+    private KlikBcaPaymentParams response;
+    private String exampleTextPositive;
+    private String exampleTextNegative;
+
+    @Before
+    public void test_setup() {
+        this.exampleTextPositive = "exampleTextPositive";
+        this.exampleTextNegative = "exampleTextNegative";
+        this.response = new KlikBcaPaymentParams(exampleTextPositive);
+    }
+
+    @Test
+    public void test_setUserId_positive() {
+        assertEquals(response.getUserId(), exampleTextPositive);
+    }
+
+    @Test
+    public void test_setUserId_negative() {
+        assertNotEquals(response.getUserId(), exampleTextNegative);
+    }
+
+}
---
+++ corekit/src/test/java/com/midtrans/sdk/corekit/models/snap/pay/request/klickbca/KlikBcaPaymentRequestTest.java
@@ -0,0 +1,37 @@
+package com.midtrans.sdk.corekit.models.snap.pay.request.klickbca;
+
+import com.midtrans.sdk.corekit.core.api.snap.model.pay.request.klikbca.KlikBcaPaymentParams;
+import com.midtrans.sdk.corekit.core.api.snap.model.pay.request.klikbca.KlikBcaPaymentRequest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+@RunWith(PowerMockRunner.class)
+public class KlikBcaPaymentRequestTest {
+    private KlikBcaPaymentRequest response;
+    private String exampleTextPositive;
+    private String exampleTextNegative;
+
+    @Before
+    public void test_setup() {
+        this.exampleTextPositive = "exampleTextPositive";
+        this.exampleTextNegative = "exampleTextNegative";
+        this.response = new KlikBcaPaymentRequest(exampleTextPositive,exampleTextPositive);
+    }
+
+    @Test
+    public void test_setKlikBcaUserId_positive() {
+        assertEquals(response.getKlikBcaUserId(), exampleTextPositive);
+    }
+
+    @Test
+    public void test_setKlikBcaUserId_negative() {
+        assertNotEquals(response.getKlikBcaUserId(), exampleTextNegative);
+    }
+
+}
