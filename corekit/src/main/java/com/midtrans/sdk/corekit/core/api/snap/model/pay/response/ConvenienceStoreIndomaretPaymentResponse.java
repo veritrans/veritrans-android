9b694d127ca86eb9ef08ae2ee890ec145bbd38b9
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-15 15:13:44 +0700

    Add conveniencestore setter

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/api/snap/model/pay/response/ConvenienceStoreIndomaretPaymentResponse.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/api/snap/model/pay/response/ConvenienceStoreIndomaretPaymentResponse.java
@@ -6,55 +6,111 @@ public class ConvenienceStoreIndomaretPaymentResponse extends BasePaymentRespons
         return statusCode;
     }
 
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
     public String getStatusMessage() {
         return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
     }
 
     public String getTransactionId() {
         return transactionId;
     }
 
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
     public String getOrderId() {
         return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
     }
 
     public String getGrossAmount() {
         return grossAmount;
     }
 
+    public void setGrossAmount(String grossAmount) {
+        this.grossAmount = grossAmount;
+    }
+
     public String getCurrency() {
         return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
     }
 
     public String getPaymentType() {
         return paymentType;
     }
 
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
     public String getTransactionTime() {
         return transactionTime;
+    }
+
+    public void setTransactionTime(String transactionTime) {
+        this.transactionTime = transactionTime;
     }
 
     public String getTransactionStatus() {
         return transactionStatus;
     }
 
+    public void setTransactionStatus(String transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+
     public String getPaymentCode() {
         return paymentCode;
+    }
+
+    public void setPaymentCode(String paymentCode){
+        this.paymentCode = paymentCode;
     }
 
     public String getStore() {
         return store;
     }
 
+    public void setStore(String store) {
+        this.store = store;
+    }
+
     public String getIndomaretExpireTime() {
         return indomaretExpireTime;
+    }
+
+    public void setIndomaretExpireTime(String indomaretExpireTime) {
+        this.indomaretExpireTime = indomaretExpireTime;
     }
 
     public String getPdfUrl() {
         return pdfUrl;
     }
 
+    public void setPdfUrl(String pdfUrl) {
+        this.pdfUrl = pdfUrl;
+    }
+
     public String getFinishRedirectUrl() {
         return finishRedirectUrl;
+    }
+    
+    public void setFinishRedirectUrl(String finishRedirectUrl) {
+        this.finishRedirectUrl = finishRedirectUrl;
     }
 }
