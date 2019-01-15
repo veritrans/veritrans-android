9e97dda1dc49b64ca8f44f69fc7395c84b35aeea
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-15 15:38:52 +0700

    Add setter to telkomsel cash response

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/api/snap/model/pay/response/EwalletTelkomselCashPaymentResponse.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/api/snap/model/pay/response/EwalletTelkomselCashPaymentResponse.java
@@ -1,47 +1,93 @@
 package com.midtrans.sdk.corekit.core.api.snap.model.pay.response;
 
 public class EwalletTelkomselCashPaymentResponse extends BasePaymentResponse {
+
     public String getStatusCode() {
         return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
     }
 
     public String getStatusMessage() {
         return statusMessage;
     }
 
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
     public String getTransactionId() {
         return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
     }
 
     public String getOrderId() {
         return orderId;
     }
 
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
     public String getGrossAmount() {
         return grossAmount;
+    }
+
+    public void setGrossAmount(String grossAmount) {
+        this.grossAmount = grossAmount;
     }
 
     public String getCurrency() {
         return currency;
     }
 
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
     public String getPaymentType() {
         return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
     }
 
     public String getTransactionTime() {
         return transactionTime;
     }
 
-    public String getTransactionStatus() {
-        return transactionStatus;
+    public void setTransactionTime(String transactionTime) {
+        this.transactionTime = transactionTime;
     }
 
-    public String getSettlementStatus() {
-        return settlementTime;
+    public String getTransactionStatus() {
+        return transactionStatus;
     }
 
     public String getFinishRedirectUrl() {
         return finishRedirectUrl;
     }
+
+    public void setTransactionStatus(String transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+
+    public void setFinishRedirectUrl(String finishRedirectUrl) {
+        this.finishRedirectUrl = finishRedirectUrl;
+    }
+
+    public String getSettlementTime() {
+        return settlementTime;
+    }
+
+    public void setSettlementTime(String settlementStatus) {
+        this.settlementTime = settlementStatus;
+    }
+
 }
