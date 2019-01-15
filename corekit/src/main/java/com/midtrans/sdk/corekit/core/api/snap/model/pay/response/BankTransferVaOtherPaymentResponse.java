40db8b06f6dad3e9df04bbf7aef1d8f971d42ec3
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-15 15:06:47 +0700

    Add setter to bank transfer response extending base response

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/api/snap/model/pay/response/BankTransferVaOtherPaymentResponse.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/api/snap/model/pay/response/BankTransferVaOtherPaymentResponse.java
@@ -10,112 +10,119 @@ public class BankTransferVaOtherPaymentResponse extends BasePaymentResponse {
         return statusCode;
     }
 
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
     public String getStatusMessage() {
         return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
     }
 
     public String getTransactionId() {
         return transactionId;
     }
 
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
     public String getOrderId() {
         return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
     }
 
     public String getGrossAmount() {
         return grossAmount;
     }
 
-    public String getCurrency() {
-        return currency;
-    }
-
-    public String getPaymentType() {
-        return paymentType;
-    }
-
-    public String getTransactionTime() {
-        return transactionTime;
-    }
-
-    public String getTransactionStatus() {
-        return transactionStatus;
-    }
-
-    public String getFraudStatus() {
-        return fraudStatus;
-    }
-
-    public List<VaNumber> getVaNumber() {
-        return vaNumbersList;
-    }
-
-    public String getBniVaNumber() {
-        return bniVaNumber;
-    }
-
-    public String getBniExpiration() {
-        return bniExpiration;
-    }
-
-    public String getPdfUrl() {
-        return pdfUrl;
-    }
-
-    public String getFinishRedirectUrl() {
-        return finishRedirectUrl;
-    }
-
-    public void setStatusCode(String statusCode){
-        this.statusCode = statusCode;
-    }
-
-    public void setStatusMessage(String statusMessage){
-        this.statusMessage = statusMessage;
-    }
-
-    public void setTransactionId(String transactionId){
-        this.transactionId = transactionId;
-    }
-
-    public void setOrderId(String orderId){
-        this.orderId = orderId;
-    }
-
     public void setGrossAmount(String grossAmount) {
         this.grossAmount = grossAmount;
+    }
+
+    public String getCurrency() {
+        return currency;
     }
 
     public void setCurrency(String currency) {
         this.currency = currency;
     }
 
+    public String getPaymentType() {
+        return paymentType;
+    }
+
     public void setPaymentType(String paymentType) {
         this.paymentType = paymentType;
+    }
+
+    public String getTransactionTime() {
+        return transactionTime;
     }
 
     public void setTransactionTime(String transactionTime) {
         this.transactionTime = transactionTime;
     }
 
+    public String getTransactionStatus() {
+        return transactionStatus;
+    }
+
     public void setTransactionStatus(String transactionStatus) {
         this.transactionStatus = transactionStatus;
     }
 
-    public void setFinishRedirectUrl(String finishRedirectUrl) {
-        this.finishRedirectUrl = finishRedirectUrl;
+    public String getFraudStatus() {
+        return fraudStatus;
+    }
+
+    public void setFraudStatus(String fraudStatus) {
+        this.fraudStatus = fraudStatus;
+    }
+
+    public List<VaNumber> getVaNumber() {
+        return vaNumbersList;
+    }
+
+    public void setVaNumbersList(List<VaNumber> vaNumbersList) {
+        this.vaNumbersList = vaNumbersList;
+    }
+
+    public String getBniVaNumber() {
+        return bniVaNumber;
     }
 
     public void setBniVaNumber(String bniVaNumber) {
         this.bniVaNumber = bniVaNumber;
     }
 
+    public String getBniExpiration() {
+        return bniExpiration;
+    }
+
     public void setBniExpiration(String bniExpiration) {
         this.bniExpiration = bniExpiration;
+    }
+
+    public String getPdfUrl() {
+        return pdfUrl;
     }
 
     public void setPdfUrl(String pdfUrl) {
         this.pdfUrl = pdfUrl;
     }
 
+    public String getFinishRedirectUrl() {
+        return finishRedirectUrl;
+    }
+
+    public void setFinishRedirectUrl(String finishRedirectUrl) {
+        this.finishRedirectUrl = finishRedirectUrl;
+    }
 }
--- corekit/src/main/java/com/midtrans/sdk/corekit/core/api/snap/model/pay/response/BankTransferVaPermataPaymentResponse.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/api/snap/model/pay/response/BankTransferVaPermataPaymentResponse.java
@@ -6,59 +6,119 @@ public class BankTransferVaPermataPaymentResponse extends BasePaymentResponse {
         return statusCode;
     }
 
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
     public String getStatusMessage() {
         return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
     }
 
     public String getTransactionId() {
         return transactionId;
     }
 
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
     public String getOrderId() {
         return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
     }
 
     public String getGrossAmount() {
         return grossAmount;
     }
 
+    public void setGrossAmount(String grossAmount) {
+        this.grossAmount = grossAmount;
+    }
+
     public String getCurrency() {
         return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
     }
 
     public String getPaymentType() {
         return paymentType;
     }
 
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
     public String getTransactionTime() {
         return transactionTime;
+    }
+
+    public void setTransactionTime(String transactionTime) {
+        this.transactionTime = transactionTime;
     }
 
     public String getTransactionStatus() {
         return transactionStatus;
     }
 
+    public void setTransactionStatus(String transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+
     public String getFraudStatus() {
         return fraudStatus;
+    }
+
+    public void setFraudStatus(String fraudStatus) {
+        this.fraudStatus = fraudStatus;
     }
 
     public String getPermataVaNumber() {
         return permataVaNumber;
     }
 
+    public void setPermataVaNumber(String permataVaNumber) {
+        this.permataVaNumber = permataVaNumber;
+    }
+
     public String getPermataExpiration() {
         return permataExpiration;
     }
 
-    public String getPdfUrl() {
-        return pdfUrl;
+    public void setPermataExpiration(String permataExpiration) {
+        this.permataExpiration = permataExpiration;
     }
 
     public String getAtmChannel() {
         return atmChannel;
     }
 
+    public void setAtmChannel(String atmChannel) {
+        this.atmChannel = atmChannel;
+    }
+
+    public String getPdfUrl() {
+        return pdfUrl;
+    }
+
+    public void setPdfUrl(String pdfUrl) {
+        this.pdfUrl = pdfUrl;
+    }
+
     public String getFinishRedirectUrl() {
         return finishRedirectUrl;
+    }
+
+    public void setFinishRedirectUrl(String finishRedirectUrl) {
+        this.finishRedirectUrl = finishRedirectUrl;
     }
 }
