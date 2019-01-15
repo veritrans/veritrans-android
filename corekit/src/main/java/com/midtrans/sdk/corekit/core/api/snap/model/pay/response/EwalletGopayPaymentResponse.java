9a62db9ac87e74b03187935866d60d828e35e0c2
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-15 15:32:21 +0700

    Add setter to gopay payment response

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/api/snap/model/pay/response/EwalletGopayPaymentResponse.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/api/snap/model/pay/response/EwalletGopayPaymentResponse.java
@@ -6,59 +6,119 @@ public class EwalletGopayPaymentResponse extends BasePaymentResponse {
         return statusCode;
     }
 
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
     public String getStatusMessage() {
         return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
     }
 
     public String getTransactionId() {
         return transactionId;
     }
 
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
     public String getOrderId() {
         return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
     }
 
     public String getGrossAmount() {
         return grossAmount;
     }
 
+    public void setGrossAmount(String grossAmount) {
+        this.grossAmount = grossAmount;
+    }
+
     public String getCurrency() {
         return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
     }
 
     public String getPaymentType() {
         return paymentType;
     }
 
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
     public String getTransactionTime() {
         return transactionTime;
+    }
+
+    public void setTransactionTime(String transactionTime) {
+        this.transactionTime = transactionTime;
     }
 
     public String getTransactionStatus() {
         return transactionStatus;
     }
 
+    public void setTransactionStatus(String transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+
     public String getFraudStatus() {
         return fraudStatus;
+    }
+
+    public void setFraudStatus(String fraudStatus) {
+        this.fraudStatus = fraudStatus;
     }
 
     public String getQrCodeUrl() {
         return qrCodeUrl;
     }
 
+    public void setQrCodeUrl(String qrCodeUrl) {
+        this.qrCodeUrl = qrCodeUrl;
+    }
+
     public String getDeeplinkUrl() {
         return deeplinkUrl;
+    }
+
+    public void setDeeplinkUrl(String deeplinkUrl) {
+        this.deeplinkUrl = deeplinkUrl;
     }
 
     public String getGopayExpiration() {
         return gopayExpiration;
     }
 
+    public void setGopayEpiration(String gopayEpiration) {
+        this.gopayExpiration = gopayEpiration;
+    }
+
     public String getGopayExpirationRaw() {
         return gopayExpirationRaw;
     }
 
+    public void setGopayExpirationRaw(String gopayExpirationRaw) {
+        this.gopayExpirationRaw = gopayExpirationRaw;
+    }
+
     public String getFinishRedirectUrl() {
         return finishRedirectUrl;
+    }
+
+    public void setFinishRedirectUrl(String finishRedirectUrl) {
+        this.finishRedirectUrl = finishRedirectUrl;
     }
 }
