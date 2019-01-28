a40559066c24970d8aa400b964b12870440ef49c
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-28 11:25:00 +0700

    Change CustomerDetails model, now using builder pattern

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/api/merchant/model/checkout/request/optional/customer/CustomerDetails.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/api/merchant/model/checkout/request/optional/customer/CustomerDetails.java
@@ -8,23 +8,21 @@ public class CustomerDetails implements Serializable {
 
     @SerializedName("first_name")
     private String firstName;
-
     @SerializedName("last_name")
     private String lastName;
     private String email;
     private String phone;
-
     @SerializedName("shipping_address")
     private Address shippingAddress;
     @SerializedName("billing_address")
     private Address billingAddress;
 
-    public CustomerDetails(String firstName,
-                           String lastName,
-                           String email,
-                           String phone,
-                           Address shippingAddress,
-                           Address billingAddress) {
+    private CustomerDetails(String firstName,
+                            String lastName,
+                            String email,
+                            String phone,
+                            Address shippingAddress,
+                            Address billingAddress) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -33,61 +31,81 @@ public class CustomerDetails implements Serializable {
         this.billingAddress = billingAddress;
     }
 
-    public CustomerDetails(String firstName,
-                           String lastName,
-                           String email,
-                           String phone) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phone = phone;
+    public static Builder builder() {
+        return new Builder();
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPhone() {
         return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 
     public Address getShippingAddress() {
         return shippingAddress;
     }
 
-    public void setShippingAddress(Address shippingAddress) {
-        this.shippingAddress = shippingAddress;
-    }
-
     public Address getBillingAddress() {
         return billingAddress;
     }
 
-    public void setBillingAddress(Address billingAddress) {
-        this.billingAddress = billingAddress;
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String phone;
+        private Address shippingAddress;
+        private Address billingAddress;
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder setShippingAddress(Address shippingAddress) {
+            this.shippingAddress = shippingAddress;
+            return this;
+        }
+
+        public Builder setBillingAddress(Address billingAddress) {
+            this.billingAddress = billingAddress;
+            return this;
+        }
+
+        public CustomerDetails build() {
+            return new CustomerDetails(
+                    firstName,
+                    lastName,
+                    email,
+                    phone,
+                    shippingAddress,
+                    billingAddress
+            );
+        }
     }
 }
