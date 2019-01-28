c14753fb514036c62184e56d628db4d976755b37
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-28 12:03:43 +0700

    Update test because new model used builder pattern

--- corekit/src/test/java/com/midtrans/sdk/corekit/models/merchant/checkout/request/optional/customer/AddressTest.java
+++ corekit/src/test/java/com/midtrans/sdk/corekit/models/merchant/checkout/request/optional/customer/AddressTest.java
@@ -16,90 +16,85 @@ public class AddressTest {
     public void test_setup() {
         this.exampleTextPositive = "exampleTextPositive";
         this.exampleTextNegative = "exampleTextNegative";
-        this.shippingAddress = new Address(null,null,null,null,null,null,null);
+        this.shippingAddress = Address
+                .builder()
+                .setFirstName(exampleTextPositive)
+                .setLastName(exampleTextPositive)
+                .setAddress(exampleTextPositive)
+                .setCity(exampleTextPositive)
+                .setPostalCode(exampleTextPositive)
+                .setPhone(exampleTextPositive)
+                .setCountryCode(exampleTextPositive)
+                .build();
     }
 
     @Test
     public void test_SetPhone_positive() {
-        shippingAddress.setPhone(exampleTextPositive);
         assertEquals(shippingAddress.getPhone(), exampleTextPositive);
     }
 
     @Test
     public void test_SetPhone_negative() {
-        shippingAddress.setPhone(exampleTextPositive);
         assertNotEquals(shippingAddress.getPhone(), exampleTextNegative);
     }
 
     @Test
     public void test_SetAddress_positive() {
-        shippingAddress.setAddress(exampleTextPositive);
         assertEquals(shippingAddress.getAddress(), exampleTextPositive);
     }
 
     @Test
     public void test_SetAddress_negative() {
-        shippingAddress.setAddress(exampleTextPositive);
         assertNotEquals(shippingAddress.getAddress(), exampleTextNegative);
     }
 
     @Test
     public void test_SetFirstName_positive() {
-        shippingAddress.setFirstName(exampleTextPositive);
         assertEquals(shippingAddress.getFirstName(), exampleTextPositive);
     }
 
     @Test
     public void test_SetFirstName_negative() {
-        shippingAddress.setFirstName(exampleTextPositive);
         assertNotEquals(shippingAddress.getFirstName(), exampleTextNegative);
     }
 
     @Test
     public void test_SetLastName_positive() {
-        shippingAddress.setLastName(exampleTextPositive);
         assertEquals(shippingAddress.getLastName(), exampleTextPositive);
     }
 
     @Test
     public void test_SetLastName_negative() {
-        shippingAddress.setLastName(exampleTextPositive);
         assertNotEquals(shippingAddress.getLastName(), exampleTextNegative);
     }
 
     @Test
     public void test_SetCountryCode_positive() {
-        shippingAddress.setCountryCode(exampleTextPositive);
         assertEquals(shippingAddress.getCountryCode(), exampleTextPositive);
     }
 
     @Test
     public void test_SetCountryCode_negative() {
-        shippingAddress.setCountryCode(exampleTextPositive);
         assertNotEquals(shippingAddress.getCountryCode(), exampleTextNegative);
     }
 
     @Test
     public void test_SetPostalCode_positive() {
-        shippingAddress.setPostalCode(exampleTextPositive);
         assertEquals(shippingAddress.getPostalCode(), exampleTextPositive);
     }
 
     @Test
     public void test_SetPostalCode_negative() {
-        shippingAddress.setPostalCode(exampleTextPositive);
         assertNotEquals(shippingAddress.getPostalCode(), exampleTextNegative);
     }
 
     @Test
     public void test_SetCity_positive() {
-        shippingAddress.setCity(exampleTextPositive);
         assertEquals(shippingAddress.getCity(), exampleTextPositive);
     }
 
     @Test
     public void test_SetCity_negative() {
-        shippingAddress.setCity(exampleTextPositive);
         assertNotEquals(shippingAddress.getCity(), exampleTextNegative);
     }
 }
