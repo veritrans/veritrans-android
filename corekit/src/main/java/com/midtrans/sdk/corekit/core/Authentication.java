14fd1a30764d9d04253fe88d444e805db7dce99b
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-29 14:54:08 +0700

    Add new enum for simplified athentication

---
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/Authentication.java
@@ -0,0 +1,12 @@
+package com.midtrans.sdk.corekit.core;
+
+/**
+ * Created by farhan.pahlevi on 29/01/19.
+ */
+
+public enum Authentication {
+    AUTH_3DS,
+    AUTH_RBA,
+    AUTH_RBA_SECURE,
+    AUTH_NONE,
+}
