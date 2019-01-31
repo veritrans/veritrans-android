27d47a87ba09fdace02a421c72cef42aa72ba8fe
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-31 12:27:49 +0700

    Change data type from enum to String

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/api/merchant/model/checkout/request/optional/CheckoutExpiry.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/api/merchant/model/checkout/request/optional/CheckoutExpiry.java
@@ -6,9 +6,7 @@ import com.midtrans.sdk.corekit.base.enums.ExpiryTimeUnit;
 
 import java.io.Serializable;
 
-import static com.midtrans.sdk.corekit.core.api.merchant.model.checkout.request.optional.TimeUnit.UNIT_DAY;
-import static com.midtrans.sdk.corekit.core.api.merchant.model.checkout.request.optional.TimeUnit.UNIT_HOUR;
-import static com.midtrans.sdk.corekit.core.api.merchant.model.checkout.request.optional.TimeUnit.UNIT_MINUTE;
+import static com.midtrans.sdk.corekit.utilities.Helper.mappingToExpiryTimeUnit;
 
 public class CheckoutExpiry implements Serializable {
 
@@ -20,7 +18,7 @@ public class CheckoutExpiry implements Serializable {
     private int duration;
 
     public CheckoutExpiry(String startTime,
-                          ExpiryTimeUnit unit,
+                          @ExpiryTimeUnit String unit,
                           int duration) {
         this.startTime = startTime;
         this.duration = duration;
@@ -35,31 +33,13 @@ public class CheckoutExpiry implements Serializable {
         this.startTime = startTime;
     }
 
-    public ExpiryTimeUnit getUnit() {
-        switch (this.unit) {
-            case UNIT_DAY:
-                return ExpiryTimeUnit.DAY;
-            case UNIT_HOUR:
-                return ExpiryTimeUnit.HOUR;
-            case UNIT_MINUTE:
-                return ExpiryTimeUnit.MINUTE;
-        }
-        return null;
+    public @ExpiryTimeUnit
+    String getUnit() {
+        return mappingToExpiryTimeUnit(unit);
     }
 
-    public void setUnit(ExpiryTimeUnit unit) {
-        switch (unit) {
-            case HOUR:
-                this.unit = UNIT_HOUR;
-                break;
-            case MINUTE:
-                this.unit = UNIT_MINUTE;
-                break;
-            case DAY:
-                this.unit = UNIT_DAY;
-                break;
-        }
-
+    public void setUnit(@ExpiryTimeUnit String unit) {
+        this.unit = mappingToExpiryTimeUnit(unit);
     }
 
     public int getDuration() {
