16da9daf46ef3a25c3c07bcfe26faad5c088fff4
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-31 12:24:51 +0700

    Change Authentication from enum to enumerated annotation

--- corekit/src/main/java/com/midtrans/sdk/corekit/base/enums/Authentication.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/base/enums/Authentication.java
@@ -1,7 +1,18 @@
 package com.midtrans.sdk.corekit.base.enums;
 
-public enum Authentication {
-    AUTH_3DS,
-    AUTH_RBA,
-    AUTH_NONE,
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import androidx.annotation.StringDef;
+
+import static com.midtrans.sdk.corekit.base.enums.Authentication.AUTH_3DS;
+import static com.midtrans.sdk.corekit.base.enums.Authentication.AUTH_NONE;
+import static com.midtrans.sdk.corekit.base.enums.Authentication.AUTH_RBA;
+
+@StringDef({AUTH_3DS, AUTH_RBA, AUTH_NONE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Authentication {
+    String AUTH_3DS = "3ds";
+    String AUTH_RBA = "rba";
+    String AUTH_NONE = "none";
 }
