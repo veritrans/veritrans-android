fb6a5cfe9b2628113af27c22a9c746eab19b22d6
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-31 12:26:38 +0700

    Change CreditCardTransactionType from enum to enumerated annotation

--- corekit/src/main/java/com/midtrans/sdk/corekit/base/enums/CreditCardTransactionType.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/base/enums/CreditCardTransactionType.java
@@ -1,6 +1,16 @@
 package com.midtrans.sdk.corekit.base.enums;
 
-public enum CreditCardTransactionType {
-    AUTHORIZE,
-    AUTHORIZE_CAPTURE
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import androidx.annotation.StringDef;
+
+import static com.midtrans.sdk.corekit.base.enums.CreditCardTransactionType.AUTHORIZE;
+import static com.midtrans.sdk.corekit.base.enums.CreditCardTransactionType.AUTHORIZE_CAPTURE;
+
+@StringDef({AUTHORIZE, AUTHORIZE_CAPTURE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface CreditCardTransactionType {
+    String AUTHORIZE = "authorize";
+    String AUTHORIZE_CAPTURE = "authorize_capture";
 }
