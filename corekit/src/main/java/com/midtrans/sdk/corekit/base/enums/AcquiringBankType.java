67deebc4bad95367c22c943eaf52a2b7216cca30
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-01-31 12:23:23 +0700

    Change AcquiringBankType from enum to enumerated annotation

--- corekit/src/main/java/com/midtrans/sdk/corekit/base/enums/AcquiringBankType.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/base/enums/AcquiringBankType.java
@@ -1,13 +1,30 @@
 package com.midtrans.sdk.corekit.base.enums;
 
-public enum AcquiringBankType {
-    CIMB,
-    BCA,
-    MANDIRI,
-    BNI,
-    BRI,
-    DANAMON,
-    MAYBANK,
-    MEGA,
-    NONE
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import androidx.annotation.StringDef;
+
+import static com.midtrans.sdk.corekit.base.enums.AcquiringBankType.BCA;
+import static com.midtrans.sdk.corekit.base.enums.AcquiringBankType.BNI;
+import static com.midtrans.sdk.corekit.base.enums.AcquiringBankType.BRI;
+import static com.midtrans.sdk.corekit.base.enums.AcquiringBankType.CIMB;
+import static com.midtrans.sdk.corekit.base.enums.AcquiringBankType.DANAMON;
+import static com.midtrans.sdk.corekit.base.enums.AcquiringBankType.MANDIRI;
+import static com.midtrans.sdk.corekit.base.enums.AcquiringBankType.MAYBANK;
+import static com.midtrans.sdk.corekit.base.enums.AcquiringBankType.MEGA;
+import static com.midtrans.sdk.corekit.base.enums.AcquiringBankType.NONE;
+
+@StringDef({CIMB, BCA, MANDIRI, BNI, BRI, DANAMON, MAYBANK, MEGA, NONE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface AcquiringBankType {
+    String CIMB = "cimb";
+    String BCA = "bca";
+    String MANDIRI = "mandiri";
+    String BNI = "bni";
+    String BRI = "bri";
+    String DANAMON = "danamon";
+    String MAYBANK = "maybank";
+    String MEGA = "mega";
+    String NONE = "offline";
 }
