530a9e28b89d2758b90d2d0f9f220a3ced6abee1
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-02-13 14:54:02 +0700

    Add ActivityHelper

--- uikit/src/main/java/com/midtrans/sdk/uikit/utilities/ActivityHelper.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/utilities/ActivityHelper.java
@@ -2,7 +2,7 @@ package com.midtrans.sdk.uikit.utilities;
 
 import android.app.Activity;
 
-public class ActivityUtilities {
+public class ActivityHelper {
     public static String getImagePath(Activity activity) {
         return "android.resource://" + activity.getPackageName() + "/";
     }
