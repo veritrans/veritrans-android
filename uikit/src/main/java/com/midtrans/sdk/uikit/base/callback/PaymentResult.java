8b1ecca0511a6092ce2045a0f2f2c0d4f45249ba
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-02-13 14:54:21 +0700

    Add serializable to paymentresult interface

--- uikit/src/main/java/com/midtrans/sdk/uikit/base/callback/PaymentResult.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/base/callback/PaymentResult.java
@@ -3,7 +3,9 @@ package com.midtrans.sdk.uikit.base.callback;
 import com.midtrans.sdk.corekit.base.enums.PaymentType;
 import com.midtrans.sdk.uikit.base.enums.PaymentStatus;
 
-public interface PaymentResult<T> {
+import java.io.Serializable;
+
+public interface PaymentResult<T> extends Serializable {
 
     void onPaymentFinished(final @PaymentStatus String statusMessage, final @PaymentType String paymentType, final T response);
 
