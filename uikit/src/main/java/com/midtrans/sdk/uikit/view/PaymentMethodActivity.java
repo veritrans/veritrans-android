f1d9bfeaf0901a9334f978919c42bd84a3c726d0
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-02-13 14:48:20 +0700

    Add Payment Method activity

--- uikit/src/main/java/com/midtrans/sdk/uikit/view/PaymentMethodActivity.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/view/PaymentMethodActivity.java
@@ -1,28 +1,166 @@
 package com.midtrans.sdk.uikit.view;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.koushikdutta.ion.Ion;
+import com.midtrans.sdk.corekit.MidtransSdk;
+import com.midtrans.sdk.corekit.base.callback.MidtransCallback;
+import com.midtrans.sdk.corekit.core.api.merchant.model.checkout.request.CheckoutTransaction;
+import com.midtrans.sdk.corekit.core.api.merchant.model.checkout.response.CheckoutWithTransactionResponse;
+import com.midtrans.sdk.corekit.core.api.snap.model.pay.response.PaymentResponse;
+import com.midtrans.sdk.corekit.core.api.snap.model.paymentinfo.PaymentInfoResponse;
+import com.midtrans.sdk.uikit.MidtransKitFlow;
 import com.midtrans.sdk.uikit.R;
 import com.midtrans.sdk.uikit.base.BaseActivity;
-import com.midtrans.sdk.uikit.utilities.ActivityUtilities;
+import com.midtrans.sdk.uikit.base.callback.PaymentResult;
+import com.midtrans.sdk.uikit.base.enums.PaymentStatus;
+import com.midtrans.sdk.uikit.utilities.ActivityHelper;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 
-public class UserDetailActivity extends BaseActivity {
+public class PaymentMethodActivity extends BaseActivity {
+
+    private PaymentResult<PaymentResponse> callback;
+    private boolean isCreditCardOnly = false;
+    private boolean isBankTransferOnly = false;
+    private boolean isBCAKlikpay = false;
+    private boolean isKlikBca = false;
+    private boolean isMandiriClickPay = false;
+    private boolean isMandiriEcash = false;
+    private boolean isCimbClicks = false;
+    private boolean isBriEpay = false;
+    private boolean isTelkomselCash = false;
+    private boolean isIndomaret = false;
+    private boolean isGopay = false;
+    private boolean isDanamonOnline = false;
+    private boolean isAkulaku = false;
+    private boolean isAlfamart = false;
+    private boolean isBcaVa = false;
+    private boolean isBniVa = false;
+    private boolean isPermataVa = false;
+    private boolean isOtherVa = false;
+    private String token = null;
+    private CheckoutTransaction checkoutTransaction = null;
+
+    private LinearLayout progressContainer;
+    private ImageView progressImage;
+    private TextView progressMessage;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_method);
+
         initToolbarAndView();
+        getIntentDataFromMidtransKitFlow();
+        checkDataBeforeStartMidtransSdk();
     }
 
     private void initToolbarAndView() {
-        setContentView(R.layout.activity_user_details);
+        progressContainer = findViewById(R.id.progress_container);
+        progressImage = findViewById(R.id.progress_bar_image);
+        progressMessage = findViewById(R.id.progress_bar_message);
 
-        ImageView progressImage = findViewById(R.id.progress_bar_image);
         Ion.with(progressImage)
-                .load(ActivityUtilities.getImagePath(this) + R.drawable.midtrans_loader);
+                .load(ActivityHelper.getImagePath(this) + R.drawable.midtrans_loader);
     }
+
+    @SuppressWarnings("unchecked")
+    private void getIntentDataFromMidtransKitFlow() {
+        callback = (PaymentResult<PaymentResponse>) getIntent().getSerializableExtra(MidtransKitFlow.INTENT_EXTRA_CALLBACK);
+        checkoutTransaction = (CheckoutTransaction) getIntent().getSerializableExtra(MidtransKitFlow.INTENT_EXTRA_TRANSACTION);
+        token = getIntent().getStringExtra(MidtransKitFlow.INTENT_EXTRA_TOKEN);
+
+        isCreditCardOnly = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_CREDIT_CARD_ONLY, false);
+        isBankTransferOnly = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_BANK_TRANSFER_ONLY, false);
+        isGopay = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_GOPAY, false);
+        isBCAKlikpay = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_BCA_KLIKPAY, false);
+        isKlikBca = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_KLIK_BCA, false);
+        isMandiriClickPay = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_MANDIRI_CLICKPAY, false);
+        isMandiriEcash = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_MANDIRI_ECASH, false);
+        isCimbClicks = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_CIMB_CLICKS, false);
+        isBriEpay = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_BRI_EPAY, false);
+        isTelkomselCash = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_TELKOMSEL_CASH, false);
+        isIndomaret = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_INDOMARET, false);
+        isDanamonOnline = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_DANAMON_ONLINE, false);
+        isAkulaku = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_AKULAKU, false);
+        isAlfamart = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_ALFAMART, false);
+
+        isBcaVa = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_BANK_TRANSFER_BCA, false);
+        isBniVa = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_BANK_TRANSFER_BNI, false);
+        isPermataVa = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_BANK_TRANSFER_PERMATA, false);
+        isOtherVa = getIntent().getBooleanExtra(MidtransKitFlow.INTENT_EXTRA_BANK_TRANSFER_OTHER, false);
+    }
+
+    private void checkDataBeforeStartMidtransSdk() {
+        showProgress();
+        if (checkoutTransaction == null && token != null) {
+            startGettingPaymentInfoWithMidtransSdk(token);
+        } else if (checkoutTransaction != null && token == null) {
+            startCheckoutWithMidtransSdk(checkoutTransaction);
+        } else {
+            setOnFailedCallback(new Throwable("Please check your input data, MidtransKit is not started."));
+        }
+    }
+
+    private void startCheckoutWithMidtransSdk(CheckoutTransaction checkoutTransaction) {
+        MidtransSdk
+                .getInstance()
+                .checkoutWithTransaction(checkoutTransaction, new MidtransCallback<CheckoutWithTransactionResponse>() {
+                    @Override
+                    public void onSuccess(CheckoutWithTransactionResponse data) {
+                        if (data.getToken() == null) {
+                            if (data.getErrorMessages() != null && !data.getErrorMessages().get(0).isEmpty()) {
+                                setOnFailedCallback(new Throwable(data.getErrorMessages().get(0)));
+                            } else {
+                                setOnFailedCallback(new Throwable("Can't create token."));
+                            }
+                        } else {
+                            startGettingPaymentInfoWithMidtransSdk(data.getToken());
+                        }
+                    }
+
+                    @Override
+                    public void onFailed(Throwable throwable) {
+                        setOnFailedCallback(throwable);
+                    }
+                });
+    }
+
+    private void startGettingPaymentInfoWithMidtransSdk(String token) {
+        MidtransSdk
+                .getInstance()
+                .getPaymentInfo(token, new MidtransCallback<PaymentInfoResponse>() {
+                    @Override
+                    public void onSuccess(PaymentInfoResponse data) {
+                        callback.onPaymentFinished(PaymentStatus.STATUS_SUCCESS, null, null);
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailed(Throwable throwable) {
+                        setOnFailedCallback(throwable);
+                    }
+                });
+    }
+
+    private void showProgress() {
+        progressContainer.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgress() {
+        progressContainer.setVisibility(View.GONE);
+    }
+
+    private void setOnFailedCallback(Throwable throwable) {
+        callback.onFailed(throwable);
+        finish();
+    }
+
 }
