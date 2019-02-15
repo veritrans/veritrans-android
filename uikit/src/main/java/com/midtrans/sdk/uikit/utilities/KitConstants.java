4616177b5c23b1dd0aba74797a7d4c2cca2eae0d
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-02-15 15:13:16 +0700

    Add constants

---
+++ uikit/src/main/java/com/midtrans/sdk/uikit/utilities/KitConstants.java
@@ -0,0 +1,17 @@
+package com.midtrans.sdk.uikit.utilities;
+
+public class KitConstants {
+
+    // response status code
+    public static final String STATUS_CODE_200 = "200";
+    public static final String STATUS_CODE_201 = "201";
+    public static final String STATUS_CODE_400 = "400";
+    public static final String STATUS_CODE_411 = "411";
+    public static final String STATUS_CODE_404 = "404";
+    public static final String STATUS_CODE_406 = "406";
+    public static final String STATUS_CODE_407 = "407";
+    public static final String STATUS_CODE_500 = "500";
+    public static final String STATUS_CODE_502 = "502";
+    public static final String STATUS_CODE_503 = "503";
+    public static final String STATUS_CODE_504 = "504";
+}
