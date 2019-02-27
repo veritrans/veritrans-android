6831433286f98221306a8e7955963116dcd54a6c
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-02-27 17:37:58 +0700

    Add property to base payment presenter

--- uikit/src/main/java/com/midtrans/sdk/uikit/base/composer/BasePaymentPresenter.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/base/composer/BasePaymentPresenter.java
@@ -4,11 +4,13 @@ import com.midtrans.sdk.corekit.base.enums.Currency;
 import com.midtrans.sdk.corekit.core.api.merchant.model.checkout.request.mandatory.TransactionDetails;
 import com.midtrans.sdk.uikit.MidtransKit;
 import com.midtrans.sdk.uikit.base.contract.BaseContract;
+import com.midtrans.sdk.uikit.base.model.PaymentResponse;
 
 public class BasePaymentPresenter<V extends BaseContract> extends BasePresenter<V> {
 
     private final String BACK_BUTTON_NAME = "Back";
     private String currency;
+    protected PaymentResponse paymentResponse;
 
     public BasePaymentPresenter() {
         super();
@@ -25,6 +27,10 @@ public class BasePaymentPresenter<V extends BaseContract> extends BasePresenter<
         } else {
             return MidtransKit.getInstance().getMidtransKitConfig().isShowPaymentStatus();
         }
+    }
+
+    public PaymentResponse getTransactionResponse() {
+        return paymentResponse;
     }
 
     public String getCurrency() {
