9cba0ae425c7c3058b63f0a653d4898704355111
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-02-28 16:14:40 +0700

    Refactor layout naming

--- uikit/src/main/java/com/midtrans/sdk/uikit/view/cimbclicks/CimbClicksInstructionActivity.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/view/cimbclicks/CimbClicksInstructionActivity.java
@@ -24,7 +24,7 @@ public class CimbClicksInstructionActivity extends BasePaymentActivity implement
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment_online_debit_cimb_clicks);
+        setContentView(R.layout.activity_payment_online_debit_cimb_clicks_instruction);
         initToolbarAndView();
         initProperties();
         initActionButton();
