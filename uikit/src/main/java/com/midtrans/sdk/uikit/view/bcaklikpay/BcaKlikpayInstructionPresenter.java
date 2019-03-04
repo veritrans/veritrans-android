af4fb99d213e1165b58e689dc0c1a183f92db62f
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-03-04 11:21:06 +0700

    Add BCA Klikpay Instructions

---
+++ uikit/src/main/java/com/midtrans/sdk/uikit/view/bcaklikpay/BcaKlikpayInstructionPresenter.java
@@ -0,0 +1,34 @@
+package com.midtrans.sdk.uikit.view.bcaklikpay;
+
+import com.midtrans.sdk.corekit.base.callback.MidtransCallback;
+import com.midtrans.sdk.corekit.core.api.snap.model.pay.response.BcaKlikpayResponse;
+import com.midtrans.sdk.corekit.core.api.snap.model.paymentinfo.PaymentInfoResponse;
+import com.midtrans.sdk.corekit.core.payment.OnlineDebitCharge;
+import com.midtrans.sdk.uikit.base.composer.BasePaymentPresenter;
+import com.midtrans.sdk.uikit.base.contract.BasePaymentContract;
+
+public class BcaKlikpayInstructionPresenter extends BasePaymentPresenter<BasePaymentContract> {
+
+    private PaymentInfoResponse paymentInfoResponse;
+
+    BcaKlikpayInstructionPresenter(BasePaymentContract view, PaymentInfoResponse paymentInfoResponse) {
+        super();
+        this.view = view;
+        this.paymentInfoResponse = paymentInfoResponse;
+    }
+
+    void startBcaKlikpayPayment(String token) {
+        OnlineDebitCharge.paymentUsingBcaKlikpay(token, new MidtransCallback<BcaKlikpayResponse>() {
+            @Override
+            public void onSuccess(BcaKlikpayResponse data) {
+                view.onPaymentSuccess(data);
+            }
+
+            @Override
+            public void onFailed(Throwable throwable) {
+                view.onPaymentError(throwable);
+            }
+        });
+    }
+
+}
