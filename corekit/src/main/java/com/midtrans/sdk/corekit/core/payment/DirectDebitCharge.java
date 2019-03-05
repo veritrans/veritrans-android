586ae7f80b91ee08cee01e63baa0464109790a6c
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-03-05 14:59:46 +0700

    Change parameter in corekit public api for Mandiri Clickpay

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/payment/DirectDebitCharge.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/payment/DirectDebitCharge.java
@@ -1,7 +1,6 @@
 package com.midtrans.sdk.corekit.core.payment;
 
 import com.midtrans.sdk.corekit.base.callback.MidtransCallback;
-import com.midtrans.sdk.corekit.core.api.snap.model.pay.request.mandiriclick.MandiriClickpayParams;
 import com.midtrans.sdk.corekit.core.api.snap.model.pay.response.KlikBcaResponse;
 import com.midtrans.sdk.corekit.core.api.snap.model.pay.response.MandiriClickpayResponse;
 
@@ -25,15 +24,19 @@ public class DirectDebitCharge extends BaseGroupPayment {
     /**
      * Start payment using bank transfer and va with Mandiri Ecash.
      *
-     * @param snapToken             token after making checkoutWithTransaction.
-     * @param mandiriClickpayParams for putting bank transfer request.
-     * @param callback              for receiving callback from request.
+     * @param snapToken      token after making checkoutWithTransaction.
+     * @param tokenId        generated tokenId
+     * @param challengeToken token from mandiri token
+     * @param input3         input3 from mandiri token
+     * @param callback       for receiving callback from request.
      */
     public static void paymentUsingMandiriClickPay(final String snapToken,
-                                                   final MandiriClickpayParams mandiriClickpayParams,
+                                                   final String tokenId,
+                                                   final String challengeToken,
+                                                   final String input3,
                                                    final MidtransCallback<MandiriClickpayResponse> callback) {
         if (isValidForNetworkCall(callback)) {
-            getSnapApiManager().paymentUsingMandiriClickPay(snapToken, mandiriClickpayParams, callback);
+            getSnapApiManager().paymentUsingMandiriClickPay(snapToken, tokenId, challengeToken, input3, callback);
         }
     }
 
