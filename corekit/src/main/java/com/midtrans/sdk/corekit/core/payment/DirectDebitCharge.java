659412c9518b7da26a4207bbf6ea19dfd469fa5b
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-03-18 13:47:52 +0700

    Change MandiriClick tokenize

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/payment/DirectDebitCharge.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/payment/DirectDebitCharge.java
@@ -1,8 +1,11 @@
 package com.midtrans.sdk.corekit.core.payment;
 
 import com.midtrans.sdk.corekit.base.callback.MidtransCallback;
+import com.midtrans.sdk.corekit.core.api.midtrans.model.cardtoken.CardTokenRequest;
+import com.midtrans.sdk.corekit.core.api.midtrans.model.tokendetails.TokenDetailsResponse;
 import com.midtrans.sdk.corekit.core.api.snap.model.pay.response.KlikBcaResponse;
 import com.midtrans.sdk.corekit.core.api.snap.model.pay.response.MandiriClickpayResponse;
+import com.midtrans.sdk.corekit.utilities.Constants;
 
 public class DirectDebitCharge extends BaseGroupPayment {
 
@@ -24,19 +27,34 @@ public class DirectDebitCharge extends BaseGroupPayment {
     /**
      * Start payment using bank transfer and va with Mandiri Ecash.
      *
-     * @param snapToken      token after making checkoutWithTransaction.
-     * @param tokenId        generated tokenId
-     * @param challengeToken token from mandiri token
-     * @param input3         input3 from mandiri token
-     * @param callback       for receiving callback from request.
+     * @param snapToken        token after making checkoutWithTransaction.
+     * @param creditCardNumber generated tokenId
+     * @param challengeToken   token from mandiri token
+     * @param input3           input3 from mandiri token
+     * @param callback         for receiving callback from request.
      */
     public static void paymentUsingMandiriClickPay(final String snapToken,
-                                                   final String tokenId,
+                                                   final String creditCardNumber,
                                                    final String challengeToken,
                                                    final String input3,
                                                    final MidtransCallback<MandiriClickpayResponse> callback) {
         if (isValidForNetworkCall(callback)) {
-            getSnapApiManager().paymentUsingMandiriClickPay(snapToken, tokenId, challengeToken, input3, callback);
+            CardTokenRequest request = new CardTokenRequest(creditCardNumber,null,null,null,getClientKey());
+            CreditCardCharge.getCardToken(request, new MidtransCallback<TokenDetailsResponse>() {
+                @Override
+                public void onSuccess(TokenDetailsResponse data) {
+                    if (data.getTokenId() != null) {
+                        getSnapApiManager().paymentUsingMandiriClickPay(snapToken, data.getTokenId(), challengeToken, input3, callback);
+                    } else {
+                        callback.onFailed(new Throwable(Constants.MESSAGE_ERROR_FAILURE_RESPONSE));
+                    }
+                }
+
+                @Override
+                public void onFailed(Throwable throwable) {
+                    callback.onFailed(throwable);
+                }
+            });
         }
     }
 
