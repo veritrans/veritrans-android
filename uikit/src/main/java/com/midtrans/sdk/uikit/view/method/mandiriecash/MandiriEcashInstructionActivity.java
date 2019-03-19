474772abe572b5edab16ec1507037711a4dd8fb7
farhanpahlevi <farhan.pahlevi@midtrans.com>
2019-03-19 18:25:26 +0700

    Fix wrong layout for mandiri ecash

--- uikit/src/main/java/com/midtrans/sdk/uikit/view/method/mandiriecash/MandiriEcashInstructionActivity.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/view/method/mandiriecash/MandiriEcashInstructionActivity.java
@@ -25,7 +25,7 @@ public class MandiriEcashInstructionActivity extends BasePaymentActivity impleme
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment_online_debit_cimb_clicks_instruction);
+        setContentView(R.layout.activity_payment_ewallet_mandiri_ecash_instruction);
         initToolbarAndView();
         initProperties();
         initActionButton();
