6b52b68130925bd1ac07e2d8495b298b81cb318d
asep.kurnia <asep.kurnia@go-jek.com>
2020-08-18 15:18:05 +0700

    add bri fragment instruction for atm internet and mobile

--- uikit/src/main/java/com/midtrans/sdk/uikit/views/banktransfer/instruction/InstructionBriVaFragment.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/views/banktransfer/instruction/InstructionBriVaFragment.java
@@ -24,15 +24,15 @@ public class InstructionBriVaFragment extends VaInstructionFragment {
 
         switch (instructionCode) {
             case UiKitConstants.INSTRUCTION_FIRST_POSITION:
-                layoutId = R.layout.fragment_instruction_bni_atm;
+                layoutId = R.layout.fragment_instruction_bri_atm;
                 break;
 
             case UiKitConstants.INSTRUCTION_SECOND_POSITION:
-                layoutId = R.layout.fragment_instruction_bni_mobile;
+                layoutId = R.layout.fragment_instruction_bri_mobile;
                 break;
 
             case UiKitConstants.INSTRUCTION_THIRD_POSITION:
-                layoutId = R.layout.fragment_instruction_bni_internet;
+                layoutId = R.layout.fragment_instruction_bri_internet;
                 break;
         }
 
