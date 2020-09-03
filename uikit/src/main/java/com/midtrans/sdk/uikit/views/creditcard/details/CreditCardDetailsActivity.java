e73e8ffa09b101a7c0f83cd017421d7de3fbb8e7
Muhammad Fauzi Masykur <muhammad.masykur@go-jek.com>
2020-09-03 17:31:53 +0700

    remove unused import

--- uikit/src/main/java/com/midtrans/sdk/uikit/views/creditcard/details/CreditCardDetailsActivity.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/views/creditcard/details/CreditCardDetailsActivity.java
@@ -15,7 +15,6 @@ import android.text.InputFilter;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
