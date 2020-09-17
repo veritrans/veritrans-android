a54f59de8466c492063060bb686fd695d008735b
jordy.jordy <jordy.jordy@go-jek.com>
2020-09-17 15:49:08 +0700

    add method checking tablet

--- uikit/src/main/java/com/midtrans/sdk/uikit/views/shopeepay/payment/ShopeePayPaymentActivity.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/views/shopeepay/payment/ShopeePayPaymentActivity.java
@@ -1,9 +1,22 @@
 package com.midtrans.sdk.uikit.views.shopeepay.payment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import com.midtrans.sdk.corekit.models.TransactionResponse;
 import com.midtrans.sdk.uikit.abstracts.BasePaymentActivity;
 
 public class ShopeePayPaymentActivity extends BasePaymentActivity implements ShopeePayPaymentView {
+
+    private Boolean isTablet;
+    private ShopeePayPaymentPresenter presenter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showProgressLayout();
+        isTablet = presenter.checkTabletDevice(this);
+
+    }
 
     @Override
     public void bindViews() {
--- uikit/src/main/java/com/midtrans/sdk/uikit/views/shopeepay/payment/ShopeePayPaymentPresenter.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/views/shopeepay/payment/ShopeePayPaymentPresenter.java
@@ -1,7 +1,12 @@
 package com.midtrans.sdk.uikit.views.shopeepay.payment;
 
+import android.app.Activity;
 import com.midtrans.sdk.uikit.abstracts.BasePaymentPresenter;
+import com.midtrans.sdk.uikit.utilities.SdkUIFlowUtil;
 
 public class ShopeePayPaymentPresenter extends BasePaymentPresenter<ShopeePayPaymentView> {
 
+    Boolean checkTabletDevice(Activity activity) {
+        return SdkUIFlowUtil.getDeviceType(activity).equals(SdkUIFlowUtil.TYPE_TABLET) && SdkUIFlowUtil.isDeviceTablet(activity);
+    }
 }
