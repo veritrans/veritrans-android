38014c3eead4e48e1a95c5428775843003c1f356
jordy.jordy <jordy.jordy@go-jek.com>
2020-09-17 19:05:25 +0700

    update on back pressed

--- uikit/src/main/java/com/midtrans/sdk/uikit/views/shopeepay/payment/ShopeePayPaymentActivity.java
+++ uikit/src/main/java/com/midtrans/sdk/uikit/views/shopeepay/payment/ShopeePayPaymentActivity.java
@@ -1,9 +1,11 @@
 package com.midtrans.sdk.uikit.views.shopeepay.payment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,12 +14,14 @@ import android.view.ViewStub;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.Toast;
+import com.midtrans.sdk.corekit.core.Logger;
 import com.midtrans.sdk.corekit.models.TransactionResponse;
 import com.midtrans.sdk.uikit.BuildConfig;
 import com.midtrans.sdk.uikit.R;
 import com.midtrans.sdk.uikit.abstracts.BasePaymentActivity;
 import com.midtrans.sdk.uikit.utilities.SdkUIFlowUtil;
 import com.midtrans.sdk.uikit.utilities.UiKitConstants;
+import com.midtrans.sdk.uikit.views.gopay.payment.GoPayPaymentActivity;
 import com.midtrans.sdk.uikit.views.gopay.status.GoPayStatusActivity;
 import com.midtrans.sdk.uikit.widgets.FancyButton;
 
@@ -72,6 +76,17 @@ public class ShopeePayPaymentActivity extends BasePaymentActivity implements Sho
             finishPayment(RESULT_OK, presenter.getTransactionResponse());
         } else if (requestCode == UiKitConstants.INTENT_CODE_SHOPEEPAY) {
             this.shopeePayIntentCode = requestCode;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isDetailShown) {
+            displayOrHideItemDetails();
+        } else if (isAlreadyGotResponse) {
+            showConfirmationDialog(getString(R.string.confirm_gopay_deeplink));
+        } else {
+            super.onBackPressed();
         }
     }
 
@@ -236,7 +251,8 @@ public class ShopeePayPaymentActivity extends BasePaymentActivity implements Sho
 
     @Override
     public void onPaymentError(Throwable error) {
-
+        hideProgressLayout();
+        showOnErrorPaymentStatusmessage(error);
     }
 
     /**
@@ -257,5 +273,34 @@ public class ShopeePayPaymentActivity extends BasePaymentActivity implements Sho
             }
         }
         return true;
+    }
+
+    private void showConfirmationDialog(String message) {
+        try {
+            AlertDialog dialog = new AlertDialog.Builder(ShopeePayPaymentActivity.this, R.style.AlertDialogCustom)
+                .setPositiveButton(R.string.text_yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (!ShopeePayPaymentActivity.this.isFinishing()) {
+                            dialog.dismiss();
+                            finishPayment(RESULT_CANCELED, presenter.getTransactionResponse());
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.text_no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (!ShopeePayPaymentActivity.this.isFinishing()) {
+                            dialog.dismiss();
+                        }
+                    }
+                })
+                .setTitle(R.string.cancel_transaction)
+                .setMessage(message)
+                .create();
+            dialog.show();
+        } catch (Exception e) {
+            Logger.e(ShopeePayPaymentActivity.class.getSimpleName(), "showDialog:" + e.getMessage());
+        }
     }
 }
