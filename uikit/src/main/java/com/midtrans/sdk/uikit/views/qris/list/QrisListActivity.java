78be40e0d4c20abd4148b5825c0de248c6b6c0a1
jordy.jordy <jordy.jordy@go-jek.com>
2020-09-22 13:55:39 +0700

    add qris list activtiy

---
+++ uikit/src/main/java/com/midtrans/sdk/uikit/views/qris/list/QrisListActivity.java
@@ -0,0 +1,22 @@
+package com.midtrans.sdk.uikit.views.qris.list;
+
+import com.midtrans.sdk.uikit.abstracts.BasePaymentActivity;
+
+public class QrisListActivity extends BasePaymentActivity implements QrisListAdapter.QrisListAdapterListener {
+    public static final String QRIS_LIST = "qris.list";
+
+    @Override
+    public void bindViews() {
+
+    }
+
+    @Override
+    public void initTheme() {
+
+    }
+
+    @Override
+    public void onItemClick(int positiion) {
+
+    }
+}
---
+++ uikit/src/main/java/com/midtrans/sdk/uikit/views/qris/list/QrisListAdapter.java
@@ -0,0 +1,8 @@
+package com.midtrans.sdk.uikit.views.qris.list;
+
+public class QrisListAdapter {
+    public interface QrisListAdapterListener {
+        void onItemClick(int positiion);
+    }
+
+}
