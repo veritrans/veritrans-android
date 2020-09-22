4518bc3b135996cf336e92e6d401c98a677f7982
jordy.jordy <jordy.jordy@go-jek.com>
2020-09-22 14:57:00 +0700

    add object for qrcode

---
+++ uikit/src/main/java/com/midtrans/sdk/uikit/models/QrCode.java
@@ -0,0 +1,43 @@
+package com.midtrans.sdk.uikit.models;
+
+public class QrCode {
+    private String type;
+    private String name;
+    private int image;
+    private Integer priority = 0;
+    private String description;
+    private String status;
+
+    public QrCode(String bankType, String name, int image, Integer priority, String description, String status) {
+        this.type = bankType;
+        this.name = name;
+        this.image = image;
+        this.priority = priority;
+        this.description = description;
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
