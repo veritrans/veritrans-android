8b95dac16fa0af4aacb560a2bd7b8fb60589928e
Ferdian Julianto <Juliantoferdian24@gmail.com>
2022-10-19 15:31:19 +0700

    feat: add GopayQris to SdkUtil; refactor: Rename ShopeePayQrisPaymentRequest to QrisPaymentRequest

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/QrisAcquirer.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/QrisAcquirer.java
@@ -3,4 +3,5 @@ package com.midtrans.sdk.corekit.core;
 public class QrisAcquirer {
 
     public static final String SHOPEEPAY = "shopeepay";
+    public static final String GOPAY = "gopay";
 }
--- corekit/src/main/java/com/midtrans/sdk/corekit/models/snap/payment/QrisPaymentRequest.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/models/snap/payment/QrisPaymentRequest.java
@@ -2,14 +2,14 @@ package com.midtrans.sdk.corekit.models.snap.payment;
 
 import com.google.gson.annotations.SerializedName;
 
-public class ShopeePayQrisPaymentRequest{
+public class QrisPaymentRequest {
     @SerializedName("payment_type")
     protected String paymentType;
 
     @SerializedName("payment_params")
     protected QrisPaymentParameter paymentParam;
 
-    public ShopeePayQrisPaymentRequest(String paymentType, QrisPaymentParameter paymentParam) {
+    public QrisPaymentRequest(String paymentType, QrisPaymentParameter paymentParam) {
         this.paymentType = paymentType;
         this.paymentParam = paymentParam;
     }
